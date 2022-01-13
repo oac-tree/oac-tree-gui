3dd71b15e356808cedd5ef11d423f8df0da3b129
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-01-13 13:26:39 +0100

    Introduce widget-based class of tests

---
+++ tests/libtestmachinery/widgetbasedtest.cpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+ *
+ * Project       : Operational Applications UI Foundation
+ *
+ * Description   : The model-view-viewmodel library of generic UI components
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2020 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "widgetbasedtest.h"
+
+#include "test_utils.h"
+
+#include <QApplication>
+
+QApplication* WidgetBasedTest::m_app = nullptr;
+
+namespace
+{
+// faking argc and argv
+char progname[] = "testview";
+char* argv[] = {&progname[0], nullptr};
+int argc = 1;
+}  // namespace
+
+void WidgetBasedTest::SetUpTestSuite()
+{
+  m_app = new QApplication(argc, argv);
+}
+
+void WidgetBasedTest::TearDownTestSuite()
+{
+  delete m_app;
+  m_app = nullptr;
+}
