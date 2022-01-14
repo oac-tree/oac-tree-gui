1d97ef6bd76b769939766bd5b27426b390ed1c8e
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-01-14 09:38:17 +0100

    Introduce clang-format machinery

---
+++ cmake/modules/CodeTools.cmake
@@ -0,0 +1,27 @@
+# Collection of functions to set-up code beautification and analysis
+include(ClangFormat)
+
+# List of targets for project code beautification.
+set(BEAUTIFICATION_TARGETS sequencerguicore testsequencerguicore testsequencerguievents)
+
+# Defines new target for 'clangformat' to beautify whole project.
+# Use 'make clangformat' or 'cmake --build . --target clangformat' to beautify the code.
+# Beautification settings are located in .clang-format in project directory.
+
+function(project_clangformat_setup)
+    set(all_sources)
+    foreach(target ${BEAUTIFICATION_TARGETS})
+        get_target_property(target_sources ${target} SOURCES)
+        list(APPEND all_sources ${target_sources})
+    endforeach()
+   clangformat_setup(${all_sources})
+endfunction()
+
+
+# Setups targets for code processing.
+
+function(project_codetools_setup)
+    if (SEQUENCERGUI_SETUP_CLANGFORMAT)
+        project_clangformat_setup()
+    endif()
+endfunction()
