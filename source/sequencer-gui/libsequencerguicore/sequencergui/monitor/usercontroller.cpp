4060afb5748d593afbb6a4c6c6affaaf692581ef
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-01-14 09:49:15 +0100

    Update file copyright header

--- source/sequencer-gui/libsequencerguicore/sequencergui/monitor/usercontroller.cpp
+++ source/sequencer-gui/libsequencerguicore/sequencergui/monitor/usercontroller.cpp
@@ -2,11 +2,11 @@
  *
  * Project       : Graphical User Interface for SUP Sequencer
  *
- * Description   : Creation, execution and monitoring of the Sequencer procedures
+ * Description   : Integrated development environment for Sequencer procedures
  *
  * Author        : Gennady Pospelov (IO)
  *
- * Copyright (c) : 2010-2020 ITER Organization,
+ * Copyright (c) : 2010-2022 ITER Organization,
  *                 CS 90 046
  *                 13067 St. Paul-lez-Durance Cedex
  *                 France
