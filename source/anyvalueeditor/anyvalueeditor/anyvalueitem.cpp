e8d0eb4e74b925fb2cef6307510b43b8acbff786
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-03-03 14:10:55 +0100

    Introduce AnyValueItem placeholder

---
+++ source/anyvalueeditor/anyvalueeditor/anyvalueitem.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "anyvalueeditor/anyvalueitem.h"
+
+namespace anyvalueeditor
+{
+
+static inline const std::string kChildren = "kChildren";
+
+AnyValueItem::AnyValueItem() : CompoundItem(Type)
+{
+  RegisterTag(mvvm::TagInfo::CreateUniversalTag(kChildren), /*as_default*/ true);
+}
+
+}  // namespace anyvalueeditor
---
+++ source/anyvalueeditor/anyvalueeditor/anyvalueitem.h
@@ -0,0 +1,40 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef ANYVALUEEDITOR_ANYVALUEITEM_H
+#define ANYVALUEEDITOR_ANYVALUEITEM_H
+
+
+#include "mvvm/model/compounditem.h"
+
+namespace anyvalueeditor
+{
+
+class AnyValueItem : public mvvm::CompoundItem
+{
+public:
+  static inline const std::string Type = "AnyValue";
+  AnyValueItem();
+
+};
+
+
+}  // namespace anyvalueeditor
+
+#endif  // ANYVALUEEDITOR_ANYVALUEITEM_H
