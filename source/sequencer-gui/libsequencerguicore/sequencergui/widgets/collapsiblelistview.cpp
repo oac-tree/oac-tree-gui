e66fa0565b50d25048278a4fd15030c48c7d3b37
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-04-07 17:50:48 +0200

    Introduce CollapsibleListView placeholder

---
+++ source/sequencer-gui/libsequencerguicore/sequencergui/widgets/collapsiblelistview.cpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "sequencergui/widgets/collapsiblelistview.h"
+
+#include <QSplitter>
+#include <QVBoxLayout>
+
+namespace sequencergui
+{
+
+CollapsibleListView::CollapsibleListView(QWidget *parent) : QWidget(parent)
+{
+  m_splitter->setOrientation(Qt::Vertical);
+
+  auto layout = new QVBoxLayout(this);
+  layout->setContentsMargins(0, 0, 0, 0);
+  layout->addWidget(m_splitter);
+}
+
+//! Adds widget to the splitter.
+//! Widget is added as it is, without collapsible bar and menus.
+void CollapsibleListView::AddWidget(QWidget *content)
+{
+  m_splitter->addWidget(content);
+}
+
+void CollapsibleListView::AddCollapsibleWidget(QWidget *content) {}
+
+}  // namespace sequencergui
---
+++ source/sequencer-gui/libsequencerguicore/sequencergui/widgets/collapsiblelistview.h
@@ -0,0 +1,51 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef SEQUENCERGUI_WIDGETS_COLLAPSIBLELISTVIEW_H
+#define SEQUENCERGUI_WIDGETS_COLLAPSIBLELISTVIEW_H
+
+#include <QWidget>
+
+class QSplitter;
+
+namespace sequencergui
+{
+
+//! Vertical widget with column of panels displayed one under another.
+//! Each panel contains user widget and can be collapsed/expanded. When expanded,
+//! the place occupied by the panel can be changed by draging a splitter.
+
+class CollapsibleListView : public QWidget
+{
+  Q_OBJECT
+
+public:
+  explicit CollapsibleListView(QWidget* parent = nullptr);
+
+  void AddWidget(QWidget* content);
+
+  void AddCollapsibleWidget(QWidget* content);
+
+private:
+  QSplitter* m_splitter{nullptr};
+};
+
+}  // namespace sequencergui
+
+#endif  // SEQUENCERGUI_WIDGETS_COLLAPSIBLELISTVIEW_H
