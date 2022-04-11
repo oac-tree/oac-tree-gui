eefbf309529425bc6d10480d653a2623aae1cf19
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-04-11 16:49:23 +0200

    Introduce MessageInterface

---
+++ source/sequencer-gui/libsequencerguicore/sequencergui/core/messageinterface.h
@@ -0,0 +1,37 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef SEQUENCERGUI_CORE_MESSAGEINTERFACE_H
+#define SEQUENCERGUI_CORE_MESSAGEINTERFACE_H
+
+#include <string>
+
+namespace sequencergui
+{
+
+//! Interface class to send messages to the user.
+class MessageInterface
+{
+public:
+  virtual void SendMessage(const std::string& text) = 0;
+};
+
+}  // namespace sequencergui
+
+#endif  // SEQUENCERGUI_CORE_MESSAGEINTERFACE_H
