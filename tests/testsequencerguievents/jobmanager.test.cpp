f9044748e9cfce16ea79cba7f626dfd46e42ba66
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-05-16 19:02:40 +0200

    Provide tests for JobManager switched to JobItem

--- tests/testsequencerguievents/jobmanager.test.cpp
+++ tests/testsequencerguievents/jobmanager.test.cpp
@@ -19,12 +19,15 @@
 
 #include "sequencergui/monitor/jobmanager.h"
 
+#include "sequencergui/model/applicationmodels.h"
 #include "sequencergui/model/instructioncontaineritem.h"
+#include "sequencergui/model/jobmodel.h"
 #include "sequencergui/model/procedureitem.h"
 #include "sequencergui/model/sequencermodel.h"
 #include "sequencergui/model/standardinstructionitems.h"
 #include "sequencergui/model/standardvariableitems.h"
 #include "sequencergui/model/workspaceitem.h"
+#include "sequencergui/model/jobitem.h"
 #include "sequencergui/monitor/jobcontext.h"
 #include "sequencergui/monitor/messagepanel.h"
 
@@ -43,26 +46,29 @@ using namespace sequencergui;
 class JobManagerTest : public ::testing::Test
 {
 public:
-  JobManagerTest() : m_model(std::make_unique<SequencerModel>())
+  JobManagerTest()
   {
     qRegisterMetaType<InstructionItem*>("InstructionItem*");
     SetupCopyProcedure();
     SetupIncludeProcedure();
+    m_job_item = m_models.GetJobModel()->InsertItem<JobItem>();
   }
 
   void SetupCopyProcedure()
   {
-    m_copy_procedure = m_model->InsertItem<ProcedureItem>(m_model->GetProcedureContainer());
-    auto copy = m_model->InsertItem<CopyItem>(m_copy_procedure->GetInstructionContainer());
+    auto model = GetSequencerModel();
+
+    m_copy_procedure = model->InsertItem<ProcedureItem>(model->GetProcedureContainer());
+    auto copy = model->InsertItem<CopyItem>(m_copy_procedure->GetInstructionContainer());
     copy->SetInput("var0");
     copy->SetOutput("var1");
 
-    auto var0 = m_model->InsertItem<LocalVariableItem>(m_copy_procedure->GetWorkspace());
+    auto var0 = model->InsertItem<LocalVariableItem>(m_copy_procedure->GetWorkspace());
     var0->SetName("var0");
     var0->SetJsonType(R"({"type":"uint32"})");
     var0->SetJsonValue("42");
 
-    auto var1 = m_model->InsertItem<LocalVariableItem>(m_copy_procedure->GetWorkspace());
+    auto var1 = model->InsertItem<LocalVariableItem>(m_copy_procedure->GetWorkspace());
     var1->SetName("var1");
     var1->SetJsonType(R"({"type":"uint32"})");
     var1->SetJsonValue("43");
@@ -70,74 +76,78 @@ public:
 
   void SetupIncludeProcedure()
   {
-    m_include_procedure = m_model->InsertItem<ProcedureItem>(m_model->GetProcedureContainer());
-    auto sequence =
-        m_model->InsertItem<SequenceItem>(m_include_procedure->GetInstructionContainer());
+    auto model = GetSequencerModel();
+    m_include_procedure = model->InsertItem<ProcedureItem>(model->GetProcedureContainer());
+    auto sequence = model->InsertItem<SequenceItem>(m_include_procedure->GetInstructionContainer());
     sequence->SetName("MySequence");
-    m_model->InsertItem<WaitItem>(sequence);
-    m_model->InsertItem<WaitItem>(sequence);
-    m_model->InsertItem<WaitItem>(sequence);
+    model->InsertItem<WaitItem>(sequence);
+    model->InsertItem<WaitItem>(sequence);
+    model->InsertItem<WaitItem>(sequence);
 
-    auto repeat = m_model->InsertItem<RepeatItem>(m_include_procedure->GetInstructionContainer());
+    auto repeat = model->InsertItem<RepeatItem>(m_include_procedure->GetInstructionContainer());
     repeat->SetRepeatCount(-1);
     repeat->SetIsRootFlag(true);
-    auto include = m_model->InsertItem<IncludeItem>(repeat);
+    auto include = model->InsertItem<IncludeItem>(repeat);
     include->SetPath("MySequence");
   }
 
-  std::unique_ptr<SequencerModel> m_model;
+  SequencerModel* GetSequencerModel() { return m_models.GetSequencerModel(); }
+  JobModel* GetJobModel() { return m_models.GetJobModel(); }
+
+  ApplicationModels m_models;
   ProcedureItem* m_include_procedure{nullptr};
   ProcedureItem* m_copy_procedure{nullptr};
+  JobItem* m_job_item{nullptr};
 };
 
-//TEST_F(JobManagerTest, InitialState)
-//{
-//  JobManager manager;
-//  manager.SetModel(m_model.get());
-//  EXPECT_FALSE(manager.GetCurrentContext());
-//}
+TEST_F(JobManagerTest, InitialState)
+{
+  JobManager manager;
+  manager.SetModel(GetJobModel());
+  EXPECT_FALSE(manager.GetCurrentContext());
+}
 
-////! Set first procedure to the JobManager and execute it.
+//! Set first procedure to the JobManager and execute it.
 
-//TEST_F(JobManagerTest, SetCurrentProcedure)
-//{
-//  MessagePanel panel;
+TEST_F(JobManagerTest, SetCurrentProcedure)
+{
+  MessagePanel panel;
+  m_job_item->SetProcedure(m_copy_procedure);
 
-//  JobManager manager;
-//  manager.SetMessagePanel(&panel);
-//  manager.SetModel(m_model.get());
-//  manager.SetCurrentJob(m_copy_procedure);
-//  manager.onChangeDelayRequest(10);
+  JobManager manager;
+  manager.SetMessagePanel(&panel);
+  manager.SetModel(GetJobModel());
+  manager.SetCurrentJob(m_job_item);
+  manager.onChangeDelayRequest(10);
 
-//  QSignalSpy spy_instruction_status(&manager, &JobManager::InstructionStatusChanged);
+  QSignalSpy spy_instruction_status(&manager, &JobManager::InstructionStatusChanged);
 
-//  auto context = manager.GetCurrentContext();
-//  ASSERT_TRUE(context != nullptr);
+  auto context = manager.GetCurrentContext();
+  ASSERT_TRUE(context != nullptr);
 
-//  EXPECT_FALSE(context->IsRunning());
-//  EXPECT_TRUE(context->GetExpandedModel());
+  EXPECT_FALSE(context->IsRunning());
 
-//  auto procedure = context->GetExpandedProcedure();
-//  ASSERT_TRUE(procedure != nullptr);
-//  EXPECT_EQ(procedure->GetInstructionContainer()->GetInstructions().size(), 1);
-//  EXPECT_EQ(procedure->GetWorkspace()->GetVariables().size(), 2);
+  auto procedure = context->GetExpandedProcedure();
+  ASSERT_TRUE(procedure != nullptr);
+  EXPECT_EQ(procedure->GetInstructionContainer()->GetInstructions().size(), 1);
+  EXPECT_EQ(procedure->GetWorkspace()->GetVariables().size(), 2);
 
-//  // starting procedure
-//  manager.onStartProcedureRequest(m_copy_procedure);
-//  EXPECT_TRUE(context->IsRunning());
+  // starting procedure
+  manager.onStartProcedureRequest(m_job_item);
+  EXPECT_TRUE(context->IsRunning());
 
-//  // We are testing here queued signals, need special waiting to let procedure complete
-//  QTest::qWait(100);
-//  EXPECT_FALSE(context->IsRunning());
-//  EXPECT_EQ(spy_instruction_status.count(), 2);
+  // We are testing here queued signals, need special waiting to let procedure complete
+  QTest::qWait(100);
+  EXPECT_FALSE(context->IsRunning());
+  EXPECT_EQ(spy_instruction_status.count(), 2);
 
-//  // variables inside are changed
-//  auto vars_inside = mvvm::utils::FindItems<LocalVariableItem>(context->GetExpandedModel());
-//  EXPECT_EQ(vars_inside.at(0)->GetJsonValue(), std::string("42"));
-//  EXPECT_EQ(vars_inside.at(1)->GetJsonValue(), std::string("42"));
+  // variables inside are changed
+  auto vars_inside = mvvm::utils::FindItems<LocalVariableItem>(GetJobModel());
+  EXPECT_EQ(vars_inside.at(0)->GetJsonValue(), std::string("42"));
+  EXPECT_EQ(vars_inside.at(1)->GetJsonValue(), std::string("42"));
 
-//  // variables at original model remained unchanged
-//  auto inside = mvvm::utils::FindItems<LocalVariableItem>(m_model.get());
-//  EXPECT_EQ(inside.at(0)->GetJsonValue(), std::string("42"));
-//  EXPECT_EQ(inside.at(1)->GetJsonValue(), std::string("43"));
-//}
+  // variables at original model remained unchanged
+  auto inside = mvvm::utils::FindItems<LocalVariableItem>(GetSequencerModel());
+  EXPECT_EQ(inside.at(0)->GetJsonValue(), std::string("42"));
+  EXPECT_EQ(inside.at(1)->GetJsonValue(), std::string("43"));
+}
