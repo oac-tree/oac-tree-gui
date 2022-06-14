cf7645abec857ffbc28e764aa1636c5c9acc824a
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-06-14 14:35:07 +0200

    Fix comment

--- source/sequencer-gui/libsequencerguicore/sequencergui/jobsystem/function_runner.h
+++ source/sequencer-gui/libsequencerguicore/sequencergui/jobsystem/function_runner.h
@@ -53,8 +53,8 @@ private:
   std::unique_ptr<FunctionRunnerImpl> p_impl;
 };
 
-//! Will wait a given amount of sec for job completion. Returns `true` is runner has finished before
-//! the timeout, `false` otherwise. Internally has a precision of 10 msec.
+//! Will wait a given amount of msec for job completion. Returns `true` is runner has finished
+//! before the timeout, `false` otherwise. Internally has a precision of 10 msec.
 bool WaitForCompletion(const FunctionRunner& runner, std::chrono::milliseconds timeout_msec);
 
 }  // namespace sequencergui
