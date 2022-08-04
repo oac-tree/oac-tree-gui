f3ef191e6d3ccc7bd13a9c9655dfc0d27a514b2a
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-08-04 14:13:00 +0200

    Provide switch between Qt5 and Qt6 in CMake machinery

--- cmake/modules/configuration.cmake
+++ cmake/modules/configuration.cmake
@@ -52,7 +52,18 @@ file(MAKE_DIRECTORY ${SEQUENCERGUI_AUTOGEN_DIR})
 set(CMAKE_AUTOMOC ON)
 set(CMAKE_AUTORCC ON)
 
-find_package(Qt5 5.12 COMPONENTS Widgets Core Gui Test REQUIRED)
+if (SEQUENCERGUI_USE_QT6)
+  find_package(QT NAMES Qt6 REQUIRED COMPONENTS Widgets Core Gui PrintSupport Test)
+else()
+  find_package(QT NAMES Qt5 REQUIRED COMPONENTS Widgets Core Gui PrintSupport Test)
+endif()
+
+find_package(Qt${QT_VERSION_MAJOR} REQUIRED COMPONENTS Widgets Core Gui PrintSupport Test)
+message(STATUS "Qt${QT_VERSION_MAJOR}.${QT_VERSION_MINOR}.${QT_VERSION_PATCH} found")
+message(STATUS " Includes: ${Qt${QT_VERSION_MAJOR}Widgets_INCLUDE_DIRS}")
+get_target_property(QtWidgets_location Qt${QT_VERSION_MAJOR}::Core LOCATION_Release)
+message(STATUS " Core library: ${QtWidgets_location}")
+
 find_package(Threads)
 
 find_package(MVVM REQUIRED)
@@ -60,10 +71,6 @@ find_package(MVVM REQUIRED)
 if (NOT SEQUENCERGUI_CODAC)
   find_package(COACompact REQUIRED)
 endif()
-
-get_target_property(Qt5Widgets_location Qt5::Widgets LOCATION_Release)
-message(STATUS " Qt5 libraries : ${Qt5Widgets_LIBRARIES} ${Qt5Widgets_location}")
-message(STATUS " Qt5 Includes  : ${Qt5Widgets_INCLUDE_DIRS}")
 
 # -----------------------------------------------------------------------------
 # Generating config files
