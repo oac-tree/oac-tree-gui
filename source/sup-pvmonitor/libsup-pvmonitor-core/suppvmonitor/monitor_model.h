ce23f68c2f5c0aff7923f30191f6a9be9f20b68b
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-11-24 14:09:39 +0100

    Placeholders for MonitorModel

---
+++ source/sup-pvmonitor/libsup-pvmonitor-core/suppvmonitor/monitor_model.h
@@ -0,0 +1,38 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef SUPPVMONITOR_MONITOR_MODEL_H_
+#define SUPPVMONITOR_MONITOR_MODEL_H_
+
+#include <mvvm/model/application_model.h>
+
+namespace suppvmonitor
+{
+
+class MonitorModel : public mvvm::ApplicationModel
+{
+
+public:
+  MonitorModel();
+
+};
+
+}  // namespace suppvmonitor
+
+#endif  // SUPPVMONITOR_MONITOR_MODEL_H_
