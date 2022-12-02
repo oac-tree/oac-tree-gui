89e41577ddcf4290cc3556efe6a3a9165d9ae599
Gennady Pospelov <gennady.pospelov@gmail.com>
2022-12-02 15:58:29 +0100

    Move AnyValue related GUI code to separate library

---
+++ source/sequencer-gui/libsequencer-gui-core/sequencergui/transform/variable_item_transform_utils.h
@@ -0,0 +1,39 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef SEQUENCERGUI_TRANSFORM_VARIABLE_ITEM_TRANSFORM_UTILS_H_
+#define SEQUENCERGUI_TRANSFORM_VARIABLE_ITEM_TRANSFORM_UTILS_H_
+
+//! @file variable_item_transform_utils.h
+//! Collection of utility functions to transform VariableItem to/from domain.
+
+#include <memory>
+
+namespace sequencergui
+{
+
+//class VariableItem;
+//class AnyValueItem;
+
+////! Updates AnyValueItem on board
+//void UpdateAnyValue(std::unique_ptr<AnyValueItem& anyvalue_item, VariableItem& variable_item);
+
+}  // namespace sequencergui
+
+#endif  // SEQUENCERGUI_TRANSFORM_VARIABLE_ITEM_TRANSFORM_UTILS_H_
