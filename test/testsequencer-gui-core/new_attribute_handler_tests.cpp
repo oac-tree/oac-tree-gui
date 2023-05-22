c42db47218fe9d8edee92381a18b27136c17bdb6
Gennady Pospelov <gennady.pospelov@iter.org>
2023-05-22 11:18:37 +0200

    Provide an alternative way to setup AnyValueScalarItem as property

--- test/testsequencer-gui-core/new_attribute_handler_tests.cpp
+++ test/testsequencer-gui-core/new_attribute_handler_tests.cpp
@@ -17,10 +17,13 @@
  * of the distribution package.
  *****************************************************************************/
 
+#include <sup/gui/model/anyvalue_conversion_utils.h>
+#include <sup/gui/model/anyvalue_item.h>
+#include <sup/gui/model/scalar_conversion_utils.h>
+
 #include <mvvm/model/compound_item.h>
 #include <mvvm/model/item_utils.h>
 
-#include <sup/gui/model/anyvalue_conversion_utils.h>
 #include <sup/sequencer/attribute_handler.h>
 
 #include <gtest/gtest.h>
@@ -29,6 +32,8 @@
  * @brief Add property to item using sequencer attribute definition.
  * @param attr Attribute definition to use for property construction.
  * @param item Compound item which will get a property on board.
+ *
+ * @details We add PropertyItem and set's it's variant to the closest type as given by AnyType.
  */
 
 void AddPropertyFromDefinition(const sup::sequencer::AttributeDefinition& attr,
@@ -41,11 +46,36 @@ void AddPropertyFromDefinition(const sup::sequencer::AttributeDefinition& attr,
   item.AddProperty(attr.GetName(), sup::gui::GetVariantForAnyValueTypeName(type_name));
 }
 
+/**
+ * @brief Add property to item using sequencer attribute definition.
+ * @param attr Attribute definition to use for property construction.
+ * @param item Compound item which will get a property on board.
+ *
+ * @details It's an alternative method which relies on AnyValueItem.
+ */
+
+// FIXME move convenience function to setup AnyValueScalarItem from AnyType to sup-gui-core
+
+void AddScalarPropertyFromDefinition(const sup::sequencer::AttributeDefinition& attr,
+                                     mvvm::CompoundItem& item)
+{
+  // In the absence of other sources of the information we can only use attribute name
+  // for both, display name and tag name of the new property item.
+  auto property = item.AddProperty<sup::gui::AnyValueScalarItem>(attr.GetName());
+  property->SetAnyTypeName(attr.GetType().GetTypeName());
+  property->SetDisplayName(attr.GetName());
+
+  auto default_anyvalue = sup::dto::AnyValue(attr.GetType());
+
+  sup::gui::SetDataFromScalar(default_anyvalue, *property);
+}
+
 class NewAttributeHandlerTest : public ::testing::Test
 {
 };
 
 //! Add property to empty CompoundItem using AttributeDefinition.
+//! We are using method AddPropertyFromStringDefinition which relies on PropertyItem.
 
 TEST_F(NewAttributeHandlerTest, AddPropertyFromStringDefinition)
 {
@@ -64,4 +94,37 @@ TEST_F(NewAttributeHandlerTest, AddPropertyFromStringDefinition)
   EXPECT_TRUE(std::holds_alternative<std::string>(property_item->Data()));
 
   EXPECT_EQ(property_item->Data<std::string>(), std::string());
+
+  // We do not have a way to convert our variant_t to AnyValue yet.
+  // It is not hard to implement it, but may be it is better to use AnyValueScalarItem as
+  // an underlying property. It is already implemented. See test below.
+}
+
+//! Add property to empty CompoundItem using AttributeDefinition.
+//! We are using method AddPropertyFromStringDefinition which relies on AnyValueScalarItem.
+
+TEST_F(NewAttributeHandlerTest, AddScalarPropertyFromDefinition)
+{
+  const std::string attribute_name("attr");
+  const sup::sequencer::AttributeDefinition attr(attribute_name, sup::dto::SignedInteger32Type);
+
+  mvvm::CompoundItem item;
+  AddScalarPropertyFromDefinition(attr, item);
+
+  // validating that CompoundItem got new property item
+  EXPECT_TRUE(mvvm::utils::HasTag(item, attribute_name));
+  auto property_item = item.GetItem({attribute_name});
+  ASSERT_NE(property_item, nullptr);
+  EXPECT_EQ(property_item->GetDisplayName(), attribute_name);
+  EXPECT_NE(dynamic_cast<sup::gui::AnyValueScalarItem*>(property_item), nullptr);
+
+  EXPECT_EQ(property_item->Data<int>(), 0);
+
+  // Checking that we can convert AnyValueScalarItem property back to AnyValue
+  sup::dto::AnyValue expected_anyvalue{sup::dto::SignedInteger32Type};
+
+  auto anyvalue_item = item.GetItem<sup::gui::AnyValueScalarItem>(attr.GetName());
+  auto any_value = GetAnyValueFromScalar(*anyvalue_item);
+
+  EXPECT_EQ(expected_anyvalue, any_value);
 }
