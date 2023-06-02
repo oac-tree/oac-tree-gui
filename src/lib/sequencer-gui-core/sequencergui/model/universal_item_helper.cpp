221901c9ad33468654973356eee367875e558cc4
Gennady Pospelov <gennady.pospelov@iter.org>
2023-06-02 15:59:44 +0200

    Add convenience function to construct universal instructions

---
+++ src/lib/sequencer-gui-core/sequencergui/model/universal_item_helper.cpp
@@ -0,0 +1,62 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2023 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "universal_item_helper.h"
+
+#include <sequencergui/core/exceptions.h>
+#include <sequencergui/domain/domain_constants.h>
+#include <sequencergui/model/universal_instruction_item.h>
+
+#include <mvvm/interfaces/sessionmodel_interface.h>
+
+namespace sequencergui
+{
+
+UniversalInstructionItem *InsertInstruction(const std::string &domain_type,
+                                            mvvm::SessionModelInterface *model)
+{
+  auto result = model->InsertItem<UniversalInstructionItem>();
+  result->SetDomainType(domain_type);
+  return result;
+}
+
+UniversalInstructionItem *InsertInstruction(const std::string &domain_type, InstructionItem *parent)
+{
+  auto model = parent->GetModel();
+  if (!model)
+  {
+    throw RuntimeException("Item doesn't belong to the model");
+  }
+
+  auto result = model->InsertItem<UniversalInstructionItem>(parent);
+  result->SetDomainType(domain_type);
+  return result;
+}
+
+void SetInput(const std::string &value, InstructionItem *item)
+{
+  item->SetProperty(domainconstants::kInputAttribute, value);
+}
+
+void SetOutput(const std::string &value, InstructionItem *item)
+{
+  item->SetProperty(domainconstants::kOutputAttribute, value);
+}
+
+}  // namespace sequencergui
---
+++ src/lib/sequencer-gui-core/sequencergui/model/universal_item_helper.h
@@ -0,0 +1,68 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2023 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef SEQUENCERGUI_MODEL_UNIVERSAL_ITEM_HELPER_H_
+#define SEQUENCERGUI_MODEL_UNIVERSAL_ITEM_HELPER_H_
+
+//! Helper functions to simplify construction of UniversalInstructionItem and UniversalVariableItem.
+
+#include <memory>
+
+namespace mvvm
+{
+class SessionModelInterface;
+}  // namespace mvvm
+
+namespace sequencergui
+{
+
+class InstructionItem;
+class UniversalInstructionItem;
+
+/**
+ * @brief Convenience function to insert universal instruction in a model.
+ * @param domain_type The domain instruction type.
+ * @param model The model to insert
+ * @return Inserted item
+ */
+UniversalInstructionItem* InsertInstruction(const std::string& domain_type,
+                                            mvvm::SessionModelInterface* model);
+
+/**
+ * @brief Convenience function to insert universal instruction in another instruction.
+ * @param domain_type The domain instruction type.
+ * @param model The model to insert
+ * @return Inserted item
+ */
+UniversalInstructionItem* InsertInstruction(const std::string& domain_type,
+                                            InstructionItem* parent);
+
+/**
+ * @brief Set input property to instruction item.
+ */
+void SetInput(const std::string& value, InstructionItem* item);
+
+/**
+ * @brief Set input property to instruction item.
+ */
+void SetOutput(const std::string& value, InstructionItem* item);
+
+}  // namespace sequencergui
+
+#endif  // SEQUENCERGUI_MODEL_SEQUENCER_UTILS_H_
