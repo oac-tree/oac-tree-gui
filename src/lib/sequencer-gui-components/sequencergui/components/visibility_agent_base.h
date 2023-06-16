1993acd33ff3a865e76219bbf71031f0bfd3bb57
Gennady Pospelov <gennady.pospelov@iter.org>
2023-06-16 17:28:12 +0200

    Placeholder for VisibilityAgentBase

---
+++ src/lib/sequencer-gui-components/sequencergui/components/visibility_agent_base.h
@@ -0,0 +1,45 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2023 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef SEQUENCERGUI_COMPONENTS_VISIBILITY_AGENT_BASE_H_
+#define SEQUENCERGUI_COMPONENTS_VISIBILITY_AGENT_BASE_H_
+
+#include <QObject>
+#include <functional>
+
+namespace sequencergui
+{
+
+class VisibilityAgentBase : public QObject
+{
+  Q_OBJECT
+
+public:
+  using callback_t = std::function<void()>;
+
+  VisibilityAgentBase(QObject* parent, callback_t subscribe, callback_t unsubscribe);
+
+private:
+  callback_t m_subscribe_callback;
+  callback_t m_unsubscribe_callback;
+};
+
+}  // namespace sequencergui
+
+#endif  // SEQUENCERGUI_COMPONENTS_VISIBILITY_AGENT_BASE_H_
