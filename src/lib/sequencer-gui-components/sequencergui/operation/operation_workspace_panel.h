892bb1b7b387f8df664bba76e912a3517f70e542
Pospelov Gennady <Gennady.Pospelov@iter.org>
2023-11-27 15:45:54 +0100

    Placeholder for OperationWorkspacePanel

---
+++ src/lib/sequencer-gui-components/sequencergui/operation/operation_workspace_panel.h
@@ -0,0 +1,47 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2023 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef SEQUENCERGUI_OPERATION_OPERATION_WORKSPACE_PANEL_H_
+#define SEQUENCERGUI_OPERATION_OPERATION_WORKSPACE_PANEL_H_
+
+#include <QWidget>
+
+namespace sequencergui
+{
+
+/**
+ * @brief The OperationWorkspacePanel class represents vertical panel on the right part of
+ * OperationMonitorView with Workspace variables.
+ *
+ * @details It is a stack of two widgets with possibility to choose either a workspace tree or
+ * workspace table.
+ */
+
+class OperationWorkspacePanel : public QWidget
+{
+  Q_OBJECT
+
+public:
+  explicit OperationWorkspacePanel(QWidget* parent = nullptr);
+  ~OperationWorkspacePanel() override;
+};
+
+}  // namespace sequencergui
+
+#endif  // SEQUENCERGUI_OPERATION_OPERATION_WORKSPACE_PANEL_H_
