a60c8a416671e83604c4da81723362fbae6883f1
Gennady Pospelov <gennady.pospelov@iter.org>
2024-02-27 16:08:56 +0100

    Inrtoduce DomainEventQueue

---
+++ src/lib/sequencer-gui-components/sequencergui/experimental/domain_event_queue.cpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "domain_event_queue.h"
+
+namespace sequencergui::experimental
+{
+
+DomainEventQueue::DomainEventQueue(QObject *parent) : QObject(parent) {}
+
+domain_event_t DomainEventQueue::PopEvent()
+{
+  domain_event_t result;
+  m_domain_events.try_pop(result);
+  return result;
+}
+
+void DomainEventQueue::PushEvent(const domain_event_t &event)
+{
+  m_domain_events.push(event);
+  emit NewEvent();
+}
+
+size_t DomainEventQueue::GetEventCount() const
+{
+  return m_domain_events.size();
+}
+
+}  // namespace sequencergui::experimental
---
+++ test/testsequencer-gui-components/domain_event_queue_tests.cpp
@@ -0,0 +1,62 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "sequencergui/experimental/domain_event_queue.h"
+
+#include <gtest/gtest.h>
+
+#include <QSignalSpy>
+
+using namespace sequencergui::experimental;
+
+//! Tests for DomainEventQueue class.
+
+class DomainEventQueueTest : public ::testing::Test
+{
+};
+
+TEST_F(DomainEventQueueTest, InitialState)
+{
+  DomainEventQueue queue;
+  EXPECT_EQ(queue.GetEventCount(), 0);
+
+  auto event = queue.PopEvent();
+  EXPECT_FALSE(IsValid(event));
+}
+
+TEST_F(DomainEventQueueTest, PushAndPop)
+{
+  DomainEventQueue queue;
+
+  QSignalSpy spy_queue(&queue, &DomainEventQueue::NewEvent);
+
+  domain_event_t event1(JobStatusChanged{"abc"});
+  domain_event_t event2(JobStatusChanged{"def"});
+
+  queue.PushEvent(event1);
+  queue.PushEvent(event2);
+
+  EXPECT_EQ(spy_queue.count(), 2);
+
+  EXPECT_EQ(queue.GetEventCount(), 2);
+  EXPECT_EQ(queue.PopEvent(), event1);
+  EXPECT_EQ(queue.GetEventCount(), 1);
+  EXPECT_EQ(queue.PopEvent(), event2);
+  EXPECT_EQ(queue.GetEventCount(), 0);
+}
