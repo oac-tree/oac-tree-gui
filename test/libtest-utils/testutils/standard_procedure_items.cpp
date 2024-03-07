f637cae3437eeec311cf9704b5acd9f6b8ead9c3
Gennady Pospelov <gennady.pospelov@iter.org>
2024-03-07 13:17:37 +0100

    Refactor test procedure creation

--- test/libtest-utils/testutils/standard_procedure_items.cpp
+++ test/libtest-utils/testutils/standard_procedure_items.cpp
@@ -37,122 +37,190 @@
 
 using namespace sequencergui;
 
+namespace
+{
+
+/**
+ * @brief Creates procedure in a model.
+ *
+ * @param procedure The procedure.
+ * @param model The model where the procedure will be created.
+ */
+sequencergui::ProcedureItem* CreateProcedure(std::unique_ptr<sequencergui::ProcedureItem> procedure,
+                                             sequencergui::SequencerModel* model)
+{
+  auto procedure_ptr = procedure.get();
+  model->InsertItem(std::move(procedure), model->GetProcedureContainer(), mvvm::TagIndex::Append());
+  return procedure_ptr;
+}
+
+/**
+ * @brief Creates procedure in a model.
+ *
+ * @param func Pointer to the procedure creation function.
+ * @param model The model where the procedure will be created.
+ */
+sequencergui::ProcedureItem* CreateProcedure(std::unique_ptr<sequencergui::ProcedureItem> (*func)(),
+                                             sequencergui::SequencerModel* model)
+{
+  return CreateProcedure(func(), model);
+}
+
+}  // namespace
+
 namespace testutils
 {
 
+std::unique_ptr<sequencergui::ProcedureItem> CreateInvalidProcedureItem()
+{
+  auto result = std::make_unique<ProcedureItem>();
+  result->GetInstructionContainer()->InsertItem<UniversalInstructionItem>(mvvm::TagIndex::Append());
+  return result;
+}
+
 sequencergui::ProcedureItem* CreateInvalidProcedureItem(sequencergui::SequencerModel* model)
 {
-  auto procedure_item = model->InsertItem<ProcedureItem>(model->GetProcedureContainer());
-  // Uninitialised instruction doesn't have counterpart in SequencerDomain
-  model->InsertItem<UniversalInstructionItem>(procedure_item->GetInstructionContainer());
-  return procedure_item;
+  return CreateProcedure(&CreateInvalidProcedureItem, model);
+}
+
+std::unique_ptr<sequencergui::ProcedureItem> CreateSingleWaitProcedureItem(
+    std::chrono::milliseconds timeout)
+{
+  auto result = std::make_unique<ProcedureItem>();
+  auto wait = result->GetInstructionContainer()->InsertItem<WaitItem>(mvvm::TagIndex::Append());
+  wait->SetTimeout(GetTimeoutInSec(timeout));
+  return result;
 }
 
 ProcedureItem* CreateSingleWaitProcedureItem(SequencerModel* model,
                                              std::chrono::milliseconds timeout)
 {
-  auto procedure_item = model->InsertItem<ProcedureItem>(model->GetProcedureContainer());
-  auto wait = model->InsertItem<WaitItem>(procedure_item->GetInstructionContainer());
-  wait->SetTimeout(GetTimeoutInSec(timeout));
-  return procedure_item;
+  return CreateProcedure(CreateSingleWaitProcedureItem(timeout), model);
+}
+
+std::unique_ptr<sequencergui::ProcedureItem> CreateCopyProcedureItem()
+{
+  auto result = std::make_unique<ProcedureItem>();
+  auto copy =
+      InsertInstruction(domainconstants::kCopyInstructionType, result->GetInstructionContainer());
+  SetInput("var0", copy);
+  SetOutput("var1", copy);
+
+  auto var0 = result->GetWorkspace()->InsertItem<LocalVariableItem>(mvvm::TagIndex::Append());
+  var0->SetName("var0");
+  SetAnyValue(sup::dto::AnyValue{sup::dto::SignedInteger32Type, 42}, *var0);
+
+  auto var1 = result->GetWorkspace()->InsertItem<LocalVariableItem>(mvvm::TagIndex::Append());
+  var1->SetName("var1");
+  SetAnyValue(sup::dto::AnyValue{sup::dto::SignedInteger32Type, 43}, *var1);
+
+  return result;
 }
 
 ProcedureItem* CreateCopyProcedureItem(SequencerModel* model)
 {
-  auto procedure_item = model->InsertItem<ProcedureItem>(model->GetProcedureContainer());
+  return CreateProcedure(&CreateCopyProcedureItem, model);
+}
 
-  auto copy = InsertInstruction(domainconstants::kCopyInstructionType,
-                                procedure_item->GetInstructionContainer());
-  SetInput("var0", copy);
-  SetOutput("var1", copy);
+std::unique_ptr<sequencergui::ProcedureItem> CreateIncludeProcedureItem()
+{
+  auto result = std::make_unique<ProcedureItem>();
+  auto sequence =
+      result->GetInstructionContainer()->InsertItem<SequenceItem>(mvvm::TagIndex::Append());
+  sequence->SetName("MySequence");
+  sequence->InsertItem<WaitItem>(mvvm::TagIndex::Append());
 
-  auto var0 = model->InsertItem<LocalVariableItem>(procedure_item->GetWorkspace());
-  var0->SetName("var0");
-  SetAnyValue(sup::dto::AnyValue{sup::dto::SignedInteger32Type, 42}, *var0);
+  auto repeat = result->GetInstructionContainer()->InsertItem<RepeatItem>(mvvm::TagIndex::Append());
+  repeat->SetRepeatCount(1);
+  repeat->SetIsRootFlag(true);
+  auto include = repeat->InsertItem<IncludeItem>(mvvm::TagIndex::Append());
+  include->SetPath("MySequence");
 
-  auto var1 = model->InsertItem<LocalVariableItem>(procedure_item->GetWorkspace());
-  var1->SetName("var1");
-  SetAnyValue(sup::dto::AnyValue{sup::dto::SignedInteger32Type, 43}, *var1);
-
-  return procedure_item;
+  return result;
 }
 
 ProcedureItem* CreateIncludeProcedureItem(SequencerModel* model)
 {
-  auto procedure_item = model->InsertItem<ProcedureItem>(model->GetProcedureContainer());
-  auto sequence = model->InsertItem<SequenceItem>(procedure_item->GetInstructionContainer());
-  sequence->SetName("MySequence");
-  model->InsertItem<WaitItem>(sequence);
-
-  auto repeat = model->InsertItem<RepeatItem>(procedure_item->GetInstructionContainer());
-  repeat->SetRepeatCount(1);
-  repeat->SetIsRootFlag(true);
-  auto include = model->InsertItem<IncludeItem>(repeat);
-  include->SetPath("MySequence");
-
-  return procedure_item;
+  return CreateProcedure(&CreateIncludeProcedureItem, model);
 }
 
-ProcedureItem* CreateInputProcedureItem(SequencerModel* model)
+std::unique_ptr<sequencergui::ProcedureItem> CreateInputProcedureItem()
 {
-  auto procedure_item = model->InsertItem<ProcedureItem>(model->GetProcedureContainer());
-  auto sequence = model->InsertItem<SequenceItem>(procedure_item->GetInstructionContainer());
+  auto result = std::make_unique<ProcedureItem>();
+  auto sequence =
+      result->GetInstructionContainer()->InsertItem<SequenceItem>(mvvm::TagIndex::Append());
   sequence->SetName("MySequence");
 
   auto input0 = InsertInstruction(domainconstants::kInputInstructionType, sequence);
   SetOutput("var1", input0);
   SetDescription("Your ID", input0);
 
-  model->InsertItem<WaitItem>(sequence);
+  sequence->InsertItem<WaitItem>(mvvm::TagIndex::Append());
 
-  auto var1 = model->InsertItem<LocalVariableItem>(procedure_item->GetWorkspace());
+  auto var1 = result->GetWorkspace()->InsertItem<LocalVariableItem>(mvvm::TagIndex::Append());
   var1->SetName("var1");
   SetAnyValue(sup::dto::AnyValue{sup::dto::SignedInteger32Type, 0}, *var1);
 
-  return procedure_item;
+  return result;
 }
 
-ProcedureItem* CreateUserChoiceProcedureItem(SequencerModel* model)
+ProcedureItem* CreateInputProcedureItem(SequencerModel* model)
+{
+  return CreateProcedure(&CreateInputProcedureItem, model);
+}
+
+std::unique_ptr<sequencergui::ProcedureItem> CreateUserChoiceProcedureItem()
 {
   // UserChoice instruction will offer the user a choice between two instructions to execute
   // 0) Wait instruction
   // 1) Copy instruction
-  auto procedure_item = model->InsertItem<ProcedureItem>(model->GetProcedureContainer());
+  auto result = std::make_unique<ProcedureItem>();
   auto userchoice = InsertInstruction(domainconstants::kUserChoiceInstructionType,
-                                      procedure_item->GetInstructionContainer());
-  auto wait0 = model->InsertItem<WaitItem>(userchoice);
+                                      result->GetInstructionContainer());
+  auto wait0 = userchoice->InsertItem<WaitItem>(mvvm::TagIndex::Append());
   wait0->SetTimeout(10.0);
 
   auto copy = InsertInstruction(domainconstants::kCopyInstructionType, userchoice);
   SetInput("var0", copy);
   SetOutput("var1", copy);
 
-  auto var0 = model->InsertItem<LocalVariableItem>(procedure_item->GetWorkspace());
+  auto var0 = result->GetWorkspace()->InsertItem<LocalVariableItem>(mvvm::TagIndex::Append());
   var0->SetName("var0");
   SetAnyValue(sup::dto::AnyValue{sup::dto::UnsignedInteger32Type, 42}, *var0);
 
-  auto var1 = model->InsertItem<LocalVariableItem>(procedure_item->GetWorkspace());
+  auto var1 = result->GetWorkspace()->InsertItem<LocalVariableItem>(mvvm::TagIndex::Append());
   var1->SetName("var1");
   SetAnyValue(sup::dto::AnyValue{sup::dto::UnsignedInteger32Type, 0}, *var1);
 
-  return procedure_item;
+  return result;
+}
+
+ProcedureItem* CreateUserChoiceProcedureItem(SequencerModel* model)
+{
+  return CreateProcedure(&CreateUserChoiceProcedureItem, model);
+}
+
+std::unique_ptr<sequencergui::ProcedureItem> CreateMessageProcedureItem(const std::string& text)
+{
+  auto result = std::make_unique<ProcedureItem>();
+  auto message = InsertInstruction(domainconstants::kMessageInstructionType,
+                                   result->GetInstructionContainer());
+  SetText(text, message);
+  return result;
 }
 
 ProcedureItem* CreateMessageProcedureItem(sequencergui::SequencerModel* model,
                                           const std::string& text)
 {
-  auto procedure_item = model->InsertItem<ProcedureItem>(model->GetProcedureContainer());
-  auto message = InsertInstruction(domainconstants::kMessageInstructionType,
-                                   procedure_item->GetInstructionContainer());
-  SetText(text, message);
-  return procedure_item;
+  return CreateProcedure(CreateMessageProcedureItem(text), model);
 }
 
-ProcedureItem* CreateVariableResetProcedureItem(sequencergui::SequencerModel* model)
+std::unique_ptr<sequencergui::ProcedureItem> CreateVariableResetProcedureItem()
 {
-  auto procedure_item = model->InsertItem<ProcedureItem>(model->GetProcedureContainer());
+  auto result = std::make_unique<ProcedureItem>();
 
-  auto sequence = model->InsertItem<SequenceItem>(procedure_item->GetInstructionContainer());
+  auto sequence =
+      result->GetInstructionContainer()->InsertItem<SequenceItem>(mvvm::TagIndex::Append());
 
   auto copy0 = InsertInstruction(domainconstants::kCopyInstructionType, sequence);
   SetInput("a", copy0);
@@ -165,18 +233,23 @@ ProcedureItem* CreateVariableResetProcedureItem(sequencergui::SequencerModel* mo
   SetInput("b", copy1);
   SetOutput("target", copy1);
 
-  auto var0 = model->InsertItem<LocalVariableItem>(procedure_item->GetWorkspace());
+  auto var0 = result->GetWorkspace()->InsertItem<LocalVariableItem>(mvvm::TagIndex::Append());
   var0->SetName("a");
   SetAnyValue(sup::dto::AnyValue{sup::dto::SignedInteger32Type, 42}, *var0);
 
-  auto var1 = model->InsertItem<LocalVariableItem>(procedure_item->GetWorkspace());
+  auto var1 = result->GetWorkspace()->InsertItem<LocalVariableItem>(mvvm::TagIndex::Append());
   var1->SetName("b");
   SetAnyValue(sup::dto::AnyValue{sup::dto::StringType, "abc"}, *var1);
 
-  auto var2 = model->InsertItem<LocalVariableItem>(procedure_item->GetWorkspace());
+  auto var2 = result->GetWorkspace()->InsertItem<LocalVariableItem>(mvvm::TagIndex::Append());
   var2->SetName("target");
 
-  return procedure_item;
+  return result;
+}
+
+ProcedureItem* CreateVariableResetProcedureItem(sequencergui::SequencerModel* model)
+{
+  return CreateProcedure(&CreateVariableResetProcedureItem, model);
 }
 
 }  // namespace testutils
--- test/libtest-utils/testutils/standard_procedure_items.h
+++ test/libtest-utils/testutils/standard_procedure_items.h
@@ -20,6 +20,7 @@
 #ifndef LIBTEST_UTILS_TESTUTILS_STANDARD_PROCEDURE_ITEMS_H_
 #define LIBTEST_UTILS_TESTUTILS_STANDARD_PROCEDURE_ITEMS_H_
 
+//! @file
 //! Collection of pocedure items for testing purpose.
 
 #include <sequencergui/domain/sequencer_types_fwd.h>
@@ -36,30 +37,105 @@ class SequencerModel;
 namespace testutils
 {
 
-//! Creates invalid procedure that will cause JobHandler to crash during the preparation.
+/**
+ * @brief Creates invalid procedure that will cause JobHandler to crash during the preparation.
+ */
+std::unique_ptr<sequencergui::ProcedureItem> CreateInvalidProcedureItem();
+
+/**
+ * @brief Creates invalid procedure that will cause JobHandler to crash during the preparation.
+ *
+ * The procedure is created in the procedure container of the given model.
+ */
 sequencergui::ProcedureItem* CreateInvalidProcedureItem(sequencergui::SequencerModel* model);
 
-//! Creates procedure with single wait instruction.
+/**
+ * @brief Creates procedure with single wait instruction.
+ */
+std::unique_ptr<sequencergui::ProcedureItem> CreateSingleWaitProcedureItem(
+    std::chrono::milliseconds timeout);
+
+/**
+ * @brief Creates procedure with single wait instruction.
+ *
+ * The procedure is created in the procedure container of the given model.
+ */
 sequencergui::ProcedureItem* CreateSingleWaitProcedureItem(sequencergui::SequencerModel* model,
                                                            std::chrono::milliseconds timeout);
 
-//! Creates procedure with two variables and single Copy instruction.
+/**
+ * @brief Creates procedure with two variables and single Copy instruction.
+ */
+std::unique_ptr<sequencergui::ProcedureItem> CreateCopyProcedureItem();
+
+/**
+ * @brief Creates procedure with two variables and single Copy instruction.
+ *
+ * The procedure is created in the procedure container of the given model.
+ */
 sequencergui::ProcedureItem* CreateCopyProcedureItem(sequencergui::SequencerModel* model);
 
-//! Creates procedure with local include.
+/**
+ * @brief Creates procedure with local include.
+ */
+std::unique_ptr<sequencergui::ProcedureItem> CreateIncludeProcedureItem();
+
+/**
+ * @brief Creates procedure with local include.
+ *
+ * The procedure is created in the procedure container of the given model.
+ */
 sequencergui::ProcedureItem* CreateIncludeProcedureItem(sequencergui::SequencerModel* model);
 
-//! Creates procedure that requires input from the user.
+/**
+ * @brief Creates procedure that requires input from the user.
+ */
+std::unique_ptr<sequencergui::ProcedureItem> CreateInputProcedureItem();
+
+/**
+ * @brief Creates procedure that requires input from the user.
+ *
+ * The procedure is created in the procedure container of the given model.
+ */
 sequencergui::ProcedureItem* CreateInputProcedureItem(sequencergui::SequencerModel* model);
 
-//! Creates procedure that requires user choice.
+/**
+ * @brief Creates procedure that requires user choice.
+ */
+std::unique_ptr<sequencergui::ProcedureItem> CreateUserChoiceProcedureItem();
+
+/**
+ * @brief Creates procedure that requires user choice.
+ *
+ * The procedure is created in the procedure container of the given model.
+ */
 sequencergui::ProcedureItem* CreateUserChoiceProcedureItem(sequencergui::SequencerModel* model);
 
-//! Creates procedure with message instruction.
+/**
+ * @brief Creates procedure with message instruction.
+ *
+ * The procedure is created in the procedure container of the given model.
+ */
+std::unique_ptr<sequencergui::ProcedureItem> CreateMessageProcedureItem(const std::string& text);
+
+/**
+ * @brief Creates procedure with message instruction.
+ *
+ * The procedure is created in the procedure container of the given model.
+ */
 sequencergui::ProcedureItem* CreateMessageProcedureItem(sequencergui::SequencerModel* model,
                                                         const std::string& text);
 
-//! Creates procedure demonstrating VariableReset instruction.
+/**
+ * @brief Creates procedure demonstrating VariableReset instruction.
+ */
+std::unique_ptr<sequencergui::ProcedureItem> CreateVariableResetProcedureItem();
+
+/**
+ * @brief Creates procedure demonstrating VariableReset instruction.
+ *
+ * The procedure is created in the procedure container of the given model.
+ */
 sequencergui::ProcedureItem* CreateVariableResetProcedureItem(sequencergui::SequencerModel* model);
 
 }  // namespace testutils
