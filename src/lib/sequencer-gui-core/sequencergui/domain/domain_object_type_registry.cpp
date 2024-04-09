b58793daa39fc793430408600b9b0b23e5f77fc6
Gennady Pospelov <gennady.pospelov@iter.org>
2024-04-09 15:16:59 +0200

    Implement DomainObjectTypeRegistry class for plugin object registration

---
+++ src/lib/sequencer-gui-core/sequencergui/domain/domain_object_type_registry.cpp
@@ -0,0 +1,43 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "domain_object_type_registry.h"
+
+namespace sequencergui
+{
+
+void DomainObjectTypeRegistry::Update(const std::string &plugin_name, const std::vector<std::string> &object_names)
+{
+  for (const auto &name : object_names)
+  {
+    auto iter = m_object_name_to_plugin_name.find(name);
+    if (iter == m_object_name_to_plugin_name.end())
+    {
+      m_object_name_to_plugin_name.insert({name, plugin_name});
+    }
+  }
+}
+
+std::optional<std::string> DomainObjectTypeRegistry::GetPluginName(const std::string &object_name) const
+{
+  auto iter = m_object_name_to_plugin_name.find(object_name);
+  return iter == m_object_name_to_plugin_name.end() ? std::optional<std::string>() : iter->second;
+}
+
+}  // namespace sequencergui
---
+++ test/testsequencer-gui-core/domain_object_type_registry_tests.cpp
@@ -0,0 +1,55 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "sequencergui/domain/domain_object_type_registry.h"
+
+#include <gtest/gtest.h>
+
+using namespace sequencergui;
+
+//! Testing DomainObjectTypeRegistry class.
+
+class DomainObjectTypeRegistryTest : public ::testing::Test
+{
+};
+
+TEST_F(DomainObjectTypeRegistryTest, Update)
+{
+  const std::string undefined("undefined");
+  DomainObjectTypeRegistry registry;
+
+  registry.Update("plugin1", {"a1", "a2"});
+  EXPECT_EQ(registry.GetPluginName("a1").value_or(undefined), "plugin1");
+  EXPECT_EQ(registry.GetPluginName("a2").value_or(undefined), "plugin1");
+  EXPECT_EQ(registry.GetPluginName("not-registered-object").value_or(undefined), undefined);
+
+  // registerging extended list of objects for another plugin name
+  registry.Update("plugin2", {"a1", "a2", "a3"});
+
+  // two previous objects ket their plugin name
+  EXPECT_EQ(registry.GetPluginName("a1").value_or(undefined), "plugin1");
+  EXPECT_EQ(registry.GetPluginName("a2").value_or(undefined), "plugin1");
+
+  // only last object got new plugin name
+  EXPECT_EQ(registry.GetPluginName("a3").value_or(undefined), "plugin2");
+
+  // empty plugin names are allowed
+  registry.Update("", {"a4"});
+  EXPECT_EQ(registry.GetPluginName("a4").value_or(undefined), "");
+}
