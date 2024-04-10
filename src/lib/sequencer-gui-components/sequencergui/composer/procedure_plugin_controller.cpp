9d8315e08fd0610537beb8a7235b97d5edcd65e0
Pospelov Gennady <Gennady.Pospelov@iter.org>
2024-04-10 15:26:00 +0200

    Implement controller to update plugin list

---
+++ src/lib/sequencer-gui-components/sequencergui/composer/procedure_plugin_controller.cpp
@@ -0,0 +1,76 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "procedure_plugin_controller.h"
+
+#include <sequencergui/model/instruction_container_item.h>
+#include <sequencergui/model/instruction_item.h>
+#include <sequencergui/model/procedure_item.h>
+#include <sequencergui/model/procedure_preamble_items.h>
+#include <sequencergui/model/workspace_item.h>
+
+#include <mvvm/model/item_utils.h>
+#include <mvvm/signals/model_listener.h>
+
+namespace sequencergui
+{
+
+ProcedurePluginController::ProcedurePluginController(mvvm::SessionModelInterface *model)
+    : m_listener(std::make_unique<listener_t>(model))
+{
+  m_listener->Connect<mvvm::ItemInsertedEvent>(this,
+                                               &ProcedurePluginController::OnItemInsertedEvent);
+  m_listener->Connect<mvvm::ItemRemovedEvent>(this, &ProcedurePluginController::OnItemRemovedEvent);
+}
+
+ProcedurePluginController::~ProcedurePluginController() = default;
+
+void ProcedurePluginController::UpdateProcedurePreamble(const mvvm::SessionItem *item)
+{
+  if (auto procedure_item = mvvm::utils::FindItemUp<ProcedureItem>(item); procedure_item)
+  {
+    UpdatePluginNames(*procedure_item);
+  }
+}
+
+bool ProcedurePluginController::IsRelevantParent(const mvvm::SessionItem *item)
+{
+  const bool is_variable_related = mvvm::utils::FindItemUp<WorkspaceItem>(item);
+  const bool is_instruction_related = mvvm::utils::FindItemUp<InstructionContainerItem>(item);
+
+  return is_instruction_related || is_variable_related;
+}
+
+void ProcedurePluginController::OnItemInsertedEvent(const mvvm::ItemInsertedEvent &event)
+{
+  if (IsRelevantParent(event.m_item))
+  {
+    UpdateProcedurePreamble(event.m_item);
+  }
+}
+
+void ProcedurePluginController::OnItemRemovedEvent(const mvvm::ItemRemovedEvent &event)
+{
+  if (IsRelevantParent(event.m_item))
+  {
+    UpdateProcedurePreamble(event.m_item);
+  }
+}
+
+}  // namespace sequencergui
---
+++ src/lib/sequencer-gui-components/sequencergui/composer/procedure_plugin_controller.h
@@ -0,0 +1,82 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef SEQUENCERGUI_COMPOSER_PROCEDURE_PLUGIN_CONTROLLER_H_
+#define SEQUENCERGUI_COMPOSER_PROCEDURE_PLUGIN_CONTROLLER_H_
+
+#include <mvvm/signals/event_types.h>
+
+#include <memory>
+
+namespace mvvm
+{
+class SessionModelInterface;
+
+template <typename T>
+class ModelListener;
+}  // namespace mvvm
+
+namespace sequencergui
+{
+
+/**
+ * @brief The ProcedurePluginController class updates plugin preamble of ProcedureItem when it sees
+ * change in used instructions and variables.
+ */
+class ProcedurePluginController
+{
+public:
+  using listener_t = mvvm::ModelListener<mvvm::SessionModelInterface>;
+
+  explicit ProcedurePluginController(mvvm::SessionModelInterface *model);
+  ~ProcedurePluginController();
+
+private:
+  /**
+   * @brief Updates procedure preamble somewhere up in the hierarchy.
+   *
+   * It is assumed, that the given item belongs to the ProcedureItem hierarchy and is related to
+   * insert/remove of instruction or variable. A call to this function will update plugin names on
+   * board of procedure preamble.
+   */
+  void UpdateProcedurePreamble(const mvvm::SessionItem *item);
+
+  /**
+   * @brief Checks if given item is related to variable Workspace, or instruction container.
+   */
+  bool IsRelevantParent(const mvvm::SessionItem *item);
+
+  /**
+   * @brief Updates procedure preamble, if necessary, when instruction or variable is inserted into
+   * the procedure.
+   */
+  void OnItemInsertedEvent(const mvvm::ItemInsertedEvent &event);
+
+  /**
+   * @brief Updates procedure preamble, if necessary, when instruction or variable is removed from
+   * the procedure.
+   */
+  void OnItemRemovedEvent(const mvvm::ItemRemovedEvent &event);
+
+  std::unique_ptr<listener_t> m_listener;
+};
+
+}  // namespace sequencergui
+
+#endif  // SEQUENCERGUI_COMPOSER_PROCEDURE_PLUGIN_CONTROLLER_H_
