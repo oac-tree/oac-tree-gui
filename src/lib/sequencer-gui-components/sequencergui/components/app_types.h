9f6b612e369d8fd7dd430f622fddfcd8457c5af1
Pospelov Gennady <Gennady.Pospelov@iter.org>
2024-08-30 14:57:58 +0200

    Add new type to distinguish workspace presentations

---
+++ src/lib/sequencer-gui-components/sequencergui/components/app_types.h
@@ -0,0 +1,37 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef SEQUENCERGUI_COMPONENTS_APP_TYPES_H_
+#define SEQUENCERGUI_COMPONENTS_APP_TYPES_H_
+
+//! @file
+//! Common types for application widgets.
+
+namespace sequencergui::app
+{
+
+enum class WorkspacePresentationType
+{
+  kWorkspaceTree,  //!< full tree with workspace variables
+  kWorkspaceTable  //!< experimental table with workspace variables
+};
+
+}  // namespace sequencergui::app
+
+#endif  // SEQUENCERGUI_COMPONENTS_APP_TYPES_H_
