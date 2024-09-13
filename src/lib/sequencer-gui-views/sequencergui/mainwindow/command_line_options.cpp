860f6f32d595fd39090641b817a9a02f802e347b
Pospelov Gennady <Gennady.Pospelov@iter.org>
2024-09-13 16:45:36 +0200

    Split command line related code to three functions

--- src/lib/sequencer-gui-views/sequencergui/mainwindow/command_line_options.cpp
+++ src/lib/sequencer-gui-views/sequencergui/mainwindow/command_line_options.cpp
@@ -28,6 +28,96 @@
 
 namespace sequencergui
 {
+
+namespace
+{
+
+const QString kInfoOption = "info";
+const QString kScaleOption = "scale";
+const QString kFontOption = "font";
+const QString kStyleOption = "style";
+const QString kFileOption = "file";
+const QString kWindowSizeOption = "size";
+
+/**
+ * @brief Populates parser with application command line options.
+ */
+void PopulateOptions(QCommandLineParser& parser)
+{
+  parser.addHelpOption();
+  parser.addVersionOption();
+
+  const QCommandLineOption info_option(kInfoOption, "Show system environment information.");
+  parser.addOption(info_option);
+
+  const QCommandLineOption scale_option(
+      "scale",
+      "Rely on system high-DPI scale via QT_ variables for 4K. If option is "
+      "absent, will rely on internal scale mechanism (preferred).");
+  parser.addOption(scale_option);
+
+  QCommandLineOption font_option(kFontOption, "Main application font point size.");
+  font_option.setValueName("pt");
+  parser.addOption(font_option);
+
+  QCommandLineOption style_option(kStyleOption,
+                                  "Main GUI style, see --info for full list available.");
+  style_option.setValueName("name");
+  parser.addOption(style_option);
+
+  const QCommandLineOption file_option(
+      {kFileOption, "f"},
+      "Load sequencer procedure from a given file name; load multiple procedures if it is a folder",
+      "filename");
+  parser.addOption(file_option);
+
+  QCommandLineOption window_size_option(kWindowSizeOption, "Initial window size");
+  window_size_option.setValueName("1024x768");
+  parser.addOption(window_size_option);
+}
+
+/**
+ * @brief Parses command line options and returns structure with options.
+ */
+Options ParseOptions(QCommandLineParser& parser)
+{
+  Options result;
+  result.info = parser.isSet(kInfoOption);
+  result.scale = parser.isSet(kScaleOption);
+
+  if (parser.isSet(kFontOption))
+  {
+    result.system_font_psize = parser.value(kFontOption).toInt();
+  }
+
+  if (parser.isSet(kStyleOption))
+  {
+    result.style = parser.value(kStyleOption);
+  }
+
+  if (parser.isSet(kFileOption))
+  {
+    result.file_name = parser.value(kFileOption);
+  }
+
+  if (parser.isSet(kWindowSizeOption))
+  {
+    if (auto optional_size = sup::gui::ParseSizeString(parser.value(kWindowSizeOption));
+        optional_size.has_value())
+    {
+      result.window_size = optional_size;
+    }
+    else
+    {
+      qInfo() << "Can't parse windows size option" << parser.value(kWindowSizeOption);
+      parser.showHelp(1);
+    }
+  }
+  return result;
+}
+
+}  // namespace
+
 Options ParseOptions(int argc, char** argv)
 {
   // Parser requires an application to be created upfront.
@@ -35,75 +125,8 @@ Options ParseOptions(int argc, char** argv)
   QCoreApplication app(argc, argv);
 
   QCommandLineParser parser;
-  parser.addHelpOption();
-  parser.addVersionOption();
-
-  QCommandLineOption info_option("info", "Show system environment information.");
-  parser.addOption(info_option);
-
-  QCommandLineOption scale_option(
-      "scale",
-      "Rely on system high-DPI scale via QT_ variables for 4K. If option is "
-      "absent, will rely on internal scale mechanism (preferred).");
-  parser.addOption(scale_option);
-
-  QCommandLineOption font_option("font", "Main application font point size.");
-  font_option.setValueName("pt");
-  parser.addOption(font_option);
-
-  QCommandLineOption style_option("style", "Main GUI style, see --info for full list available.");
-  style_option.setValueName("name");
-  parser.addOption(style_option);
-
-  QCommandLineOption file_option(
-      {"f", "file"},
-      "Load sequencer procedure from a given file name; load multiple procedures if it is a folder",
-      "filename");
-  parser.addOption(file_option);
-
-  QCommandLineOption window_size_option("size", "Initial window size");
-  window_size_option.setValueName("1024x768");
-  parser.addOption(window_size_option);
-
+  PopulateOptions(parser);
   parser.process(app);
-
-  Options result;
-  result.info = parser.isSet(info_option);
-  result.scale = parser.isSet(scale_option);
-
-  if (parser.isSet(font_option))
-  {
-    result.system_font_psize = parser.value(font_option).toInt();
-  }
-
-  if (parser.isSet(style_option))
-  {
-    result.style = parser.value(style_option);
-  }
-
-  if (parser.isSet(file_option))
-  {
-    result.file_name = parser.value(file_option);
-  }
-
-  if (parser.isSet(file_option))
-  {
-    result.file_name = parser.value(file_option);
-  }
-
-  if (parser.isSet(window_size_option))
-  {
-    if (auto optional_size = sup::gui::ParseSizeString(parser.value(window_size_option));
-        optional_size.has_value())
-    {
-      result.window_size = optional_size;
-    }
-    else
-    {
-      qInfo() << "Can't parse windows size option" << parser.value(window_size_option);
-      parser.showHelp(1);
-    }
-  }
-  return result;
+  return ParseOptions(parser);
 }
 }  // namespace sequencergui
