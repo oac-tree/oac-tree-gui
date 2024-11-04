abada1b43d15fe82253a442bdccab916872723d7
Pospelov Gennady <Gennady.Pospelov@iter.org>
2024-11-04 15:39:03 +0100

    Rename JobHandler->AbstractJobHandler
    And make original JobHandler nearly empty, and derived from AbstractobHandler

--- src/lib/sequencer-gui-components/sequencergui/jobsystem/abstract_job_handler.cpp
+++ src/lib/sequencer-gui-components/sequencergui/jobsystem/abstract_job_handler.cpp
@@ -19,9 +19,275 @@
 
 #include "abstract_job_handler.h"
 
+#include "domain_event_dispatcher_context.h"
+#include "job_log.h"
+#include "local_domain_runner.h"
+
+#include <sequencergui/core/exceptions.h>
+#include <sequencergui/model/instruction_container_item.h>
+#include <sequencergui/model/instruction_item.h>
+#include <sequencergui/model/item_constants.h>
+#include <sequencergui/model/iterate_helper.h>
+#include <sequencergui/model/job_item.h>
+#include <sequencergui/model/job_model.h>
+#include <sequencergui/model/procedure_item.h>
+#include <sequencergui/operation/breakpoint_controller.h>
+#include <sequencergui/operation/breakpoint_helper.h>
+#include <sequencergui/pvmonitor/workspace_item_listener.h>
+#include <sequencergui/pvmonitor/workspace_monitor_helper.h>
+#include <sequencergui/transform/domain_procedure_builder.h>
+#include <sequencergui/transform/procedure_item_builder.h>
+
+#include <mvvm/signals/item_listener.h>
+
+#include <sup/sequencer/procedure.h>
+#include <sup/sequencer/workspace.h>
+
+#include <QDebug>
+
 namespace sequencergui
 {
 
+AbstractJobHandler::AbstractJobHandler(JobItem *job_item, const UserContext &user_context)
+    : m_guiobject_builder(std::make_unique<ProcedureItemBuilder>())
+    , m_job_log(new JobLog)
+    , m_job_item(job_item)
+{
+  if (!job_item)
+  {
+    throw RuntimeException("JobItem is not initialised");
+  }
+
+  if (!m_job_item->GetProcedure())
+  {
+    throw RuntimeException("Procedure doesn't exist");
+  }
+
+  m_property_listener = std::make_unique<mvvm::ItemListener>(job_item);
+  auto on_event = [this](const mvvm::PropertyChangedEvent &event)
+  {
+    if (event.name == itemconstants::kTickTimeout)
+    {
+      m_domain_runner_service->SetTickTimeout(m_job_item->GetTickTimeout());
+    }
+  };
+  m_property_listener->Connect<mvvm::PropertyChangedEvent>(on_event);
+
+  SetupBreakpointController();
+
+  CreateDomainProcedure();
+  auto domain_procedure_ptr = m_domain_procedure.get();
+
+  SetupDomainRunner(user_context, m_job_item->GetTickTimeout());  // calls also Procedure::Setup
+
+  SetupExpandedProcedureItem(domain_procedure_ptr);
+}
+
 AbstractJobHandler::~AbstractJobHandler() = default;
 
+void AbstractJobHandler::OnStartRequest()
+{
+  m_domain_runner_service->Start();
 }
+
+void AbstractJobHandler::OnPauseRequest()
+{
+  m_domain_runner_service->Pause();
+}
+
+void AbstractJobHandler::OnMakeStepRequest()
+{
+  m_domain_runner_service->Step();
+}
+
+void AbstractJobHandler::OnStopRequest()
+{
+  m_domain_runner_service->Stop();
+}
+
+void AbstractJobHandler::Reset()
+{
+  m_domain_runner_service->Reset();
+}
+
+bool AbstractJobHandler::IsRunning() const
+{
+  return m_domain_runner_service->IsBusy();
+}
+
+ProcedureItem *AbstractJobHandler::GetExpandedProcedure() const
+{
+  return m_job_item->GetExpandedProcedure();
+}
+
+RunnerStatus AbstractJobHandler::GetRunnerStatus() const
+{
+  return m_domain_runner_service ? static_cast<RunnerStatus>(m_domain_runner_service->GetJobState())
+                                 : RunnerStatus::kInitial;
+}
+
+JobLog *AbstractJobHandler::GetJobLog() const
+{
+  return m_job_log;
+}
+
+void AbstractJobHandler::OnToggleBreakpointRequest(InstructionItem *instruction)
+{
+  if (IsRunning())
+  {
+    return;
+  }
+  ToggleBreakpointStatus(*instruction);
+
+  // update domain breakpoint
+  const size_t instruction_index = m_guiobject_builder->GetIndex(instruction);
+  SetDomainBreakpoint(instruction_index, GetBreakpointStatus(*instruction));
+}
+
+void AbstractJobHandler::OnInstructionStateUpdated(const InstructionStateUpdatedEvent &event)
+{
+  if (auto *item = m_guiobject_builder->GetInstruction(event.index); item)
+  {
+    item->SetStatus(::sup::sequencer::StatusToString(event.state.m_execution_status));
+    emit InstructionStatusChanged(item);
+  }
+  else
+  {
+    qWarning() << "Error in ProcedureReporter: can't find domain instruction counterpart";
+  }
+}
+
+void AbstractJobHandler::OnJobStateChanged(const JobStateChangedEvent &event)
+{
+  m_job_item->SetStatus(::sup::sequencer::ToString(event.status));
+}
+
+void AbstractJobHandler::onLogEvent(const sequencergui::LogEvent &event)
+{
+  m_job_log->Append(event);
+}
+
+void AbstractJobHandler::OnNextLeavesChangedEvent(const NextLeavesChangedEvent &event)
+{
+  std::vector<InstructionItem *> items;
+  for (auto instruction_index : event.leaves)
+  {
+    if (auto *item = m_guiobject_builder->GetInstruction(instruction_index); item)
+    {
+      items.push_back(item);
+    }
+    else
+    {
+      qWarning() << "Error in ProcedureReporter: can't find domain instruction counterpart";
+    }
+  }
+
+  emit NextLeavesChanged(items);
+}
+
+void AbstractJobHandler::OnVariableUpdatedEvent(const VariableUpdatedEvent &event)
+{
+  m_workspace_item_listener->ProcessEventFromDomain(event);
+}
+
+JobModel *AbstractJobHandler::GetJobModel()
+{
+  return dynamic_cast<JobModel *>(m_job_item->GetModel());
+}
+
+void AbstractJobHandler::SetupBreakpointController()
+{
+  m_breakpoint_controller = std::make_unique<BreakpointController>();
+
+  if (auto expanded_procedure = GetExpandedProcedure(); expanded_procedure)
+  {
+    m_breakpoint_controller->SaveBreakpoints(*expanded_procedure);
+  }
+}
+
+void AbstractJobHandler::CreateDomainProcedure()
+{
+  m_domain_procedure = DomainProcedureBuilder::CreateProcedure(*m_job_item->GetProcedure());
+}
+
+void AbstractJobHandler::SetupExpandedProcedureItem(procedure_t *domain_procedure)
+{
+  // We expect that Procedure::Setup was already called
+
+  // remove previous expanded procedure
+  if (auto expanded_procedure = GetExpandedProcedure(); expanded_procedure)
+  {
+    GetJobModel()->RemoveItem(expanded_procedure);
+  }
+
+  auto expanded_procedure =
+      m_guiobject_builder->CreateProcedureItem(domain_procedure, /*root_only*/ true);
+  auto expanded_procedure_ptr = expanded_procedure.get();
+
+  GetJobModel()->InsertItem(std::move(expanded_procedure), m_job_item, mvvm::TagIndex::Append());
+  m_breakpoint_controller->RestoreBreakpoints(*expanded_procedure_ptr);
+
+  PropagateBreakpointsToDomain();
+
+  m_workspace_item_listener = std::make_unique<WorkspaceItemListener>(
+      expanded_procedure_ptr->GetWorkspace(), &domain_procedure->GetWorkspace());
+}
+
+void AbstractJobHandler::SetupDomainRunner(const UserContext &user_context, int sleep_time_msec)
+{
+  // this creates beneath DomainRunner(AsyncRunner(Runner())) and then call procedure->Setup
+  // m_domain_runner_service =
+  //     std::make_unique<DomainRunnerService>(CreateContext(), user_context, *m_domain_procedure);
+  m_domain_runner_service = std::make_unique<LocalDomainRunner>(CreateContext(), user_context,
+                                                                std::move(m_domain_procedure));
+  m_domain_runner_service->SetTickTimeout(sleep_time_msec);
+}
+
+void AbstractJobHandler::SetDomainBreakpoint(size_t index, BreakpointStatus breakpoint_status)
+{
+  if (breakpoint_status == BreakpointStatus::kSet)
+  {
+    m_domain_runner_service->SetBreakpoint(index);
+  }
+  else
+  {
+    // We do not use "disabled" breakpoints in the domain, InstructionItem's breakpoint marked as
+    // disabled, will remove breakpoint from the domain
+    m_domain_runner_service->RemoveBreakpoint(index);
+  }
+}
+
+void AbstractJobHandler::PropagateBreakpointsToDomain()
+{
+  // visit all insrtuction items and set breakpoint status to the domain
+  auto func = [this](const InstructionItem *item)
+  {
+    auto index = m_guiobject_builder->GetIndex(item);
+    SetDomainBreakpoint(index, GetBreakpointStatus(*item));
+  };
+  IterateInstructionContainer<const InstructionItem *>(
+      GetExpandedProcedure()->GetInstructionContainer()->GetInstructions(), func);
+}
+
+DomainEventDispatcherContext AbstractJobHandler::CreateContext()
+{
+  DomainEventDispatcherContext result;
+
+  result.process_instruction_state_updated = [this](const InstructionStateUpdatedEvent &event)
+  { OnInstructionStateUpdated(event); };
+
+  result.process_variable_updated = [this](const VariableUpdatedEvent &event)
+  { OnVariableUpdatedEvent(event); };
+
+  result.process_job_state_changed = [this](const JobStateChangedEvent &event)
+  { OnJobStateChanged(event); };
+
+  result.process_log_event = [this](const LogEvent &event) { onLogEvent(event); };
+
+  result.next_leaves_changed_event = [this](const NextLeavesChangedEvent &event)
+  { OnNextLeavesChangedEvent(event); };
+
+  return result;
+}
+
+}  // namespace sequencergui
--- src/lib/sequencer-gui-components/sequencergui/jobsystem/job_handler.h
+++ src/lib/sequencer-gui-components/sequencergui/jobsystem/job_handler.h
@@ -20,212 +20,21 @@
 #ifndef SEQUENCERGUI_JOBSYSTEM_JOB_HANDLER_H_
 #define SEQUENCERGUI_JOBSYSTEM_JOB_HANDLER_H_
 
-#include <sequencergui/domain/sequencer_types_fwd.h>
-#include <sequencergui/jobsystem/domain_events.h>
-#include <sequencergui/jobsystem/runner_status.h>
-#include <sequencergui/jobsystem/user_context.h>
-#include <sequencergui/operation/breakpoint_types.h>
-
-#include <QObject>
-#include <memory>
-
-namespace mvvm
-{
-class ItemListener;
-}
+#include <sequencergui/jobsystem/abstract_job_handler.h>
 
 namespace sequencergui
 {
 
-class ProcedureItem;
-class JobLog;
-class JobItem;
-class InstructionItem;
-class ProcedureItemBuilder;
-class LogEvent;
-class WorkspaceItemListener;
-class JobModel;
-class BreakpointController;
-class DomainRunnerService;
-class LocalDomainRunner;
-class DomainEventDispatcherContext;
-
 /**
- * @brief The JobHandler class is the main class to run a job represented by the JobItem.
- *
- * It is used by the JobManager, where each JobItem is handled by its own JobHandler. JobHandler has
- * multiple responsibilities:
- *
- * - generate sequencer domain procedure using ProcedureItem
- * - setup domain procedure, and generate corresponding expanded ProcedureItem
- * - handle start/stop/pause/step requests
- * - listen for all sequencer domain events and update GUI items accordingly
+ * @brief The AbstractJobHandler is a base class to run a jobs represented by the JobItem.
  */
-class JobHandler : public QObject
+class JobHandler : public AbstractJobHandler
 {
   Q_OBJECT
 
 public:
   explicit JobHandler(JobItem* job_item, const UserContext& user_context = {});
   ~JobHandler() override;
-
-  /**
-   * @brief Run sequencer procedure in continuous manner, if allowed.
-   */
-  void OnStartRequest();
-
-  /**
-   * @brief Pause procedure's execution.
-   */
-  void OnPauseRequest();
-
-  /**
-   * @brief Execute a single step of the procedure, if allowed.
-   */
-  void OnMakeStepRequest();
-
-  /**
-   * @brief Stops the procedure execution.
-   */
-  void OnStopRequest();
-
-  /**
-   * @brief Reset not-running procedure to initial state.
-   *
-   * Call domains' AsyncRunner::Reset
-   */
-  void Reset();
-
-  /**
-   * @brief Checks if the job is running, which is one of pause/stepping/running states.
-   */
-  bool IsRunning() const;
-
-  /**
-   * @brief Returns expanded ProcedureItem.
-   */
-  ProcedureItem* GetExpandedProcedure() const;
-
-  /**
-   * @brief Returns runner status.
-   */
-  RunnerStatus GetRunnerStatus() const;
-
-  /**
-   * @brief Returns a job log.
-   */
-  JobLog* GetJobLog() const;
-
-  /**
-   * @brief Set/unset a breakpoint for given instruction.
-   *
-   * It is used to handle double-clicks in a tree of instructions belonging to ExpandedProcedure
-   * item. Each double-click will toggle breakpoint status in a circle set/disabled/not-set.
-   *
-   * @param instruction Instruction item from expanded procedure tree.
-   *
-   */
-  void OnToggleBreakpointRequest(sequencergui::InstructionItem* instruction);
-
-signals:
-  void InstructionStatusChanged(sequencergui::InstructionItem* instruction);
-  void NextLeavesChanged(const std::vector<sequencergui::InstructionItem*>&);
-
-private:
-  /**
-   * @brief Processes instruction status change in the domain, and update InstructionItem's status
-   * accordingly.
-   */
-  void OnInstructionStateUpdated(const InstructionStateUpdatedEvent& event);
-
-  /**
-   * @brief Processes job status changed from the domain, and update JobItem status accordingly.
-   */
-  void OnJobStateChanged(const JobStateChangedEvent& event);
-
-  /**
-   * @brief Processes log events from the domain, and put them in GUI JobLog.
-   */
-  void onLogEvent(const sequencergui::LogEvent& event);
-
-  /**
-   * @brief Handles events reporting for changes in domain's next leaves.
-   */
-  void OnNextLeavesChangedEvent(const NextLeavesChangedEvent& event);
-
-  /**
-   * @brief Handles events reporting update in the domain variable.
-   */
-  void OnVariableUpdatedEvent(const VariableUpdatedEvent& event);
-
-  /**
-   * @brief Returns job model to which our JobItem
-   */
-  JobModel* GetJobModel();
-
-  /**
-   * @brief Setups breakpoint controller.
-   */
-  void SetupBreakpointController();
-
-  /**
-   * @brief Creates domain procedure from ProcedureItem.
-   */
-  void CreateDomainProcedure();
-
-  /**
-   * @brief Setup expanded procedure item.
-   *
-   * It will reflect the content of domain procedure after its Setup.
-   */
-  void SetupExpandedProcedureItem(procedure_t* domain_procedure);
-
-  /**
-   * @brief Setups the domain runner.
-   *
-   * @param user_context The context to provide interaction with the user.
-   * @param sleep_time_msec Sleep time at the end of each tick.
-   */
-  void SetupDomainRunner(const UserContext& user_context, int sleep_time_msec);
-
-  /**
-   * @brief Set breakpoint to the domain instruction with the given index.
-   */
-  void SetDomainBreakpoint(size_t index, BreakpointStatus breakpoint_status);
-
-  /**
-   * @brief Propagates breakpoints from expanded procedure to domain.
-   */
-  void PropagateBreakpointsToDomain();
-
-  /**
-   * @brief Returns a context necessary for sequencer event queue to function.
-   */
-  DomainEventDispatcherContext CreateContext();
-
-  //!< GUI object builder holding domain/GUI object correspondance
-  std::unique_ptr<ProcedureItemBuilder> m_guiobject_builder;
-
-  //!< domain procedure should live longer than the runner
-  std::unique_ptr<procedure_t> m_domain_procedure;
-
-  //!< main runner to start/stop jobs
-  std::unique_ptr<LocalDomainRunner> m_domain_runner_service;
-
-  //!< dedicated listener to provide communication between domain/GUI workspace variables
-  std::unique_ptr<WorkspaceItemListener> m_workspace_item_listener;
-
-  //!< main controller to handle breakpoints toggling
-  std::unique_ptr<BreakpointController> m_breakpoint_controller;
-
-  //!< listens for JobItem property change
-  std::unique_ptr<mvvm::ItemListener> m_property_listener;
-
-  //!< the job log
-  JobLog* m_job_log{nullptr};
-
-  //!< the JobItem being handled
-  JobItem* m_job_item{nullptr};
 };
 
 }  // namespace sequencergui
