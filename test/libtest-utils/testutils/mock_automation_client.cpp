1882de95998a0542478af6819d7a6062696417ac
Pospelov Gennady <Gennady.Pospelov@iter.org>
2024-11-22 15:50:09 +0100

    Implement MockAutomationClient

---
+++ test/libtest-utils/testutils/mock_automation_client.cpp
@@ -0,0 +1,71 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "mock_automation_client.h"
+
+#include <sequencergui/jobsystem/abstract_job_handler.h>
+
+namespace testutils
+{
+
+AutomationClientDecorator::AutomationClientDecorator(IAutomationClient &decoratee)
+    : m_decoratee(decoratee)
+{
+}
+
+std::string AutomationClientDecorator::GetServerName() const
+{
+  return m_decoratee.GetServerName();
+}
+
+size_t AutomationClientDecorator::GetJobCount() const
+{
+  return m_decoratee.GetJobCount();
+}
+
+std::string AutomationClientDecorator::GetProcedureName(size_t job_index) const
+{
+  return m_decoratee.GetProcedureName(job_index);
+}
+
+std::unique_ptr<sequencergui::AbstractJobHandler> AutomationClientDecorator::CreateJobHandler(
+    sequencergui::RemoteJobItem *job_item, const sequencergui::UserContext &user_context)
+{
+  return m_decoratee.CreateJobHandler(job_item, user_context);
+}
+
+std::unique_ptr<sequencergui::IAutomationClient> CreateAutomationClientDecorator(
+    sequencergui::IAutomationClient &decoratee)
+{
+  return std::make_unique<AutomationClientDecorator>(decoratee);
+}
+
+std::function<std::unique_ptr<sequencergui::IAutomationClient>(const std::string &server_name)>
+AutomationClientDecoratorCreateFunc(sequencergui::IAutomationClient &decoratee)
+{
+  auto result = [&decoratee](const std::string &server_name)
+  {
+    (void)server_name;
+    return CreateAutomationClientDecorator(decoratee);
+  };
+
+  return result;
+}
+
+}  // namespace testutils
---
+++ test/libtest-utils/testutils/mock_automation_client.h
@@ -0,0 +1,85 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP Sequencer
+ *
+ * Description   : Integrated development environment for Sequencer procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#ifndef LIBTEST_UTILS_TESTUTILS_MOCK_AUTOMATION_CLIENT_H_
+#define LIBTEST_UTILS_TESTUTILS_MOCK_AUTOMATION_CLIENT_H_
+
+#include <sequencergui/jobsystem/i_automation_client.h>
+
+#include <gmock/gmock.h>
+
+#include <functional>
+
+namespace testutils
+{
+
+/**
+ * @brief The MockAutomationClient class helps to test RemoteConnectionService.
+ */
+class MockAutomationClient : public sequencergui::IAutomationClient
+{
+public:
+  MOCK_METHOD(std::string, GetServerName, (), (const, override));
+  MOCK_METHOD(size_t, GetJobCount, (), (const, override));
+  MOCK_METHOD(std::string, GetProcedureName, (size_t), (const, override));
+  MOCK_METHOD(std::unique_ptr<sequencergui::AbstractJobHandler>, CreateJobHandler,
+              (sequencergui::RemoteJobItem*, const sequencergui::UserContext&), (override));
+};
+
+/**
+ * @brief The AutomationClientDecorator class forwards all calls to underlying IAutomationClient
+ * class.
+ *
+ * It is used in situations when we have to use unique_ptr<IAutomationClient>, and do not want to
+ * loose an ownerhsip on gtest mocking object.
+ */
+class AutomationClientDecorator : public sequencergui::IAutomationClient
+{
+public:
+  AutomationClientDecorator(sequencergui::IAutomationClient& decoratee);
+
+  std::string GetServerName() const override;
+
+  size_t GetJobCount() const override;
+
+  std::string GetProcedureName(size_t job_index) const override;
+
+  std::unique_ptr<sequencergui::AbstractJobHandler> CreateJobHandler(
+      sequencergui::RemoteJobItem* job_item,
+      const sequencergui::UserContext& user_context) override;
+
+private:
+  sequencergui::IAutomationClient& m_decoratee;
+};
+
+/**
+ * @brief Creates simple forward decorator around mocking object.
+ */
+std::unique_ptr<sequencergui::IAutomationClient> CreateAutomationClientDecorator(
+    sequencergui::IAutomationClient& decoratee);
+
+/**
+ * @brief Creates factory functions to create decorators around mocking objects.
+ */
+std::function<std::unique_ptr<sequencergui::IAutomationClient>(const std::string& server_name)>
+AutomationClientDecoratorCreateFunc(sequencergui::IAutomationClient& decoratee);
+
+}  // namespace testutils
+
+#endif  // LIBTEST_UTILS_TESTUTILS_MOCK_AUTOMATION_CLIENT_H_
