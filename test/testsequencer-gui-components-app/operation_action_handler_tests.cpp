e5e33c3c17691626214a81ea46b87a5d63474122
Gennady Pospelov <gennady.pospelov@iter.org>
2024-11-26 09:41:23 +0100

    Move test to the folder with event loop for QSignalSpy
    Add unit test for two remote jobs consecutive insert

--- test/testsequencer-gui-components-app/operation_action_handler_tests.cpp
+++ test/testsequencer-gui-components-app/operation_action_handler_tests.cpp
@@ -25,11 +25,16 @@
 
 #include <mvvm/model/application_model.h>
 #include <mvvm/model/model_utils.h>
+#include <mvvm/test/test_helper.h>
 
 #include <gtest/gtest.h>
 #include <testutils/mock_job_manager.h>
 #include <testutils/mock_operation_action_context.h>
 #include <testutils/mock_remote_connection_service.h>
+
+#include <QSignalSpy>
+
+Q_DECLARE_METATYPE(sequencergui::JobItem*)
 
 namespace sequencergui
 {
@@ -76,6 +81,9 @@ TEST_F(OperationActionHandlerTest, OnImportRemoteJobRequest)
   EXPECT_CALL(m_mock_operation_context, OnGetRemoteConnectionContext());
   EXPECT_CALL(m_mock_mock_job_manager, SubmitJob(::testing::_));
 
+  QSignalSpy spy_selected_request(operation_handler.get(),
+                                  &OperationActionHandler::MakeJobSelectedRequest);
+
   EXPECT_TRUE(operation_handler->OnImportRemoteJobRequest());
 
   // as a result of import request, a single RemoteJobItem has been inserted into the model
@@ -83,6 +91,55 @@ TEST_F(OperationActionHandlerTest, OnImportRemoteJobRequest)
   ASSERT_NE(job_item, nullptr);
   EXPECT_EQ(job_item->GetServerName(), server_name);
   EXPECT_EQ(job_item->GetRemoteJobIndex(), job_index);
+
+  EXPECT_EQ(mvvm::test::GetSendItem<JobItem*>(spy_selected_request), job_item);
+}
+
+//! Testing import of two remote jobs between two existing one.
+TEST_F(OperationActionHandlerTest, ImportOfTwoRemoteJobs)
+{
+  auto job_item0 = m_model.InsertItem<LocalJobItem>();
+  auto job_item1 = m_model.InsertItem<LocalJobItem>();
+
+  const std::string server_name("abc");
+  const size_t job_index0{42};
+  const size_t job_index1{43};
+  const RemoteConnectionContext connection_context{server_name, {job_index0, job_index1}};
+
+  // setting up context, so it report "user choice" related to remote job import
+  ON_CALL(m_mock_operation_context, OnGetRemoteConnectionContext())
+      .WillByDefault(::testing::Return(std::optional<RemoteConnectionContext>(connection_context)));
+  // job_item0 will be always reported as selected
+  ON_CALL(m_mock_operation_context, OnSelectedJob()).WillByDefault(::testing::Return(job_item0));
+
+  auto operation_handler = CreateOperationHandler();
+
+  EXPECT_CALL(m_mock_operation_context, OnSelectedJob()).Times(2);
+  EXPECT_CALL(m_mock_operation_context, OnGetRemoteConnectionContext()).Times(1);
+  EXPECT_CALL(m_mock_mock_job_manager, SubmitJob(::testing::_)).Times(2);
+
+  QSignalSpy spy_selected_request(operation_handler.get(),
+                                  &OperationActionHandler::MakeJobSelectedRequest);
+
+  EXPECT_TRUE(operation_handler->OnImportRemoteJobRequest());
+
+  // as a result of import request, two RemoteJobItem have been inserted into the model
+  // item order: job_item0, item_inserted_second, item_inserted_fisr, job_item1
+  auto inserted_job_items = mvvm::utils::GetTopItems<RemoteJobItem>(&m_model);
+  auto item_inserted_first = inserted_job_items.at(1);
+  auto item_inserted_second = inserted_job_items.at(0);
+  ASSERT_EQ(inserted_job_items.size(), 2);
+  EXPECT_EQ(item_inserted_first->GetServerName(), server_name);
+  EXPECT_EQ(item_inserted_first->GetRemoteJobIndex(), job_index0);
+  EXPECT_EQ(item_inserted_second->GetServerName(), server_name);
+  EXPECT_EQ(item_inserted_second->GetRemoteJobIndex(), job_index1);
+
+  EXPECT_EQ(spy_selected_request.count(), 2);
+
+  // items were inserted in this order since job_item0 will always reported as selected
+  const std::vector<JobItem*> expected_items(
+      {job_item0, item_inserted_second, item_inserted_first, job_item1});
+  EXPECT_EQ(mvvm::utils::GetTopItems<JobItem>(&m_model), expected_items);
 }
 
 }  // namespace sequencergui
