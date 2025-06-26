725fe6cb8cd98cf39a28d9f8ba1c51d2c1207294
Gennady Pospelov <gennady.pospelov@iter.org>
2025-06-26 16:38:19 +0200

    Test confirming that "connected" green rectangle works for VariableInfo

--- test/testoac-tree-gui-components/workspace_operation_viewmodel_tests.cpp
+++ test/testoac-tree-gui-components/workspace_operation_viewmodel_tests.cpp
@@ -22,6 +22,7 @@
 
 #include <oac_tree_gui/components/component_helper.h>
 #include <oac_tree_gui/domain/domain_helper.h>
+#include <oac_tree_gui/model/sequencer_item_helper.h>
 #include <oac_tree_gui/model/standard_variable_items.h>
 #include <oac_tree_gui/model/variable_info_item.h>
 #include <oac_tree_gui/model/workspace_item.h>
@@ -238,6 +239,70 @@ TEST_F(WorkspaceOperationViewModelTest, ChannelAccessIsAvailableStatus)
 
   QSignalSpy spy_data_changed(&viewmodel, &WorkspaceOperationViewModel::dataChanged);
   variable_item->SetIsAvailable(false);
+
+  ASSERT_EQ(spy_data_changed.count(), 1);
+  const QList<QVariant> arguments = spy_data_changed.takeFirst();
+  EXPECT_EQ(arguments.size(), 3);  // QModelIndex left, QModelIndex right, QVector<int> roles
+  EXPECT_EQ(arguments.at(0).value<QModelIndex>(), variable_channel_index);
+  EXPECT_EQ(arguments.at(1).value<QModelIndex>(), variable_channel_index);
+  const QVector<int> expectedRoles = {Qt::DecorationRole};
+  EXPECT_EQ(arguments.at(2).value<QVector<int>>(), expectedRoles);
+
+  EXPECT_EQ(viewmodel.data(variable_channel_index, Qt::DecorationRole).value<QColor>(),
+            GetDisonnectedVariableColor());
+}
+
+TEST_F(WorkspaceOperationViewModelTest, ChannelAccessIsAvailableStatusInfoItemCase)
+{
+  if (!IsSequencerPluginEpicsAvailable())
+  {
+    GTEST_SKIP();
+  }
+
+  // Creating domain Variable, then VariableInfo, then VariableInfoItem
+  const std::string expected_name("abc");
+  const std::string expected_channel("CHANNEL");
+  const std::string expected_datatype(R"RAW({"type":"int32"})RAW");
+  const std::string expected_value("42");
+
+  const attribute_list_t attributes = {{domainconstants::kNameAttribute, expected_name},
+                                       {domainconstants::kChannelAttribute, expected_channel},
+                                       {domainconstants::kTypeAttribute, expected_datatype},
+                                       {domainconstants::kValueAttribute, expected_value}};
+
+  auto variable_item =
+      CreateVariableInfoItem(domainconstants::kPvAccessServerVariableType, 0, attributes);
+  auto variable_item_ptr = variable_item.get();
+
+  mvvm::ApplicationModel model;
+
+  auto workspace_item = model.InsertItem<WorkspaceItem>();
+
+  (void)model.InsertItem(std::move(variable_item), workspace_item, mvvm::TagIndex::Append());
+
+  GetIsAvailableItem(*variable_item_ptr)->SetData(true);
+
+  WorkspaceOperationViewModel viewmodel(&model);
+  viewmodel.SetRootSessionItem(workspace_item);
+  EXPECT_EQ(viewmodel.rowCount(), 1);
+  EXPECT_EQ(viewmodel.columnCount(), 4);
+
+  // checking viewmodel layout
+  EXPECT_EQ(viewmodel.rowCount(), 1);
+  EXPECT_EQ(viewmodel.columnCount(), 4);
+
+  // reading data from cell
+  auto variable_channel_index = viewmodel.index(0, 3);
+  EXPECT_FALSE(viewmodel.data(variable_channel_index, Qt::EditRole).isValid());
+  EXPECT_TRUE(viewmodel.data(variable_channel_index, Qt::DisplayRole).isValid());
+  EXPECT_EQ(viewmodel.data(variable_channel_index, Qt::DisplayRole).toString(),
+            QString::fromStdString(expected_channel));
+  EXPECT_TRUE(viewmodel.data(variable_channel_index, Qt::DecorationRole).isValid());
+  EXPECT_EQ(viewmodel.data(variable_channel_index, Qt::DecorationRole).value<QColor>(),
+            GetConnectedVariableColor());
+
+  QSignalSpy spy_data_changed(&viewmodel, &WorkspaceOperationViewModel::dataChanged);
+  variable_item_ptr->SetIsAvailable(false);
 
   ASSERT_EQ(spy_data_changed.count(), 1);
   const QList<QVariant> arguments = spy_data_changed.takeFirst();
