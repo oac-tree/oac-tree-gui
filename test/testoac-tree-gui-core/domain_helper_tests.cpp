6b03e85754556251172f13deeded667bf72c80d1
Pospelov Gennady <Gennady.Pospelov@iter.org>
2025-07-02 13:49:58 +0200

    Rename unit test file

--- test/testoac-tree-gui-core/domain_helper_tests.cpp
+++ test/testoac-tree-gui-core/domain_helper_tests.cpp
@@ -18,43 +18,161 @@
  * of the distribution package.
  *****************************************************************************/
 
-#include <oac_tree_gui/domain/domain_helper.h>
+#include "oac_tree_gui/domain/domain_helper.h"
 
-#include <mvvm/core/platform.h>
+#include <oac_tree_gui/domain/domain_object_type_registry.h>
+
+#include <sup/oac-tree/constants.h>
+#include <sup/oac-tree/instruction.h>
+#include <sup/oac-tree/user_interface.h>
+#include <sup/oac-tree/variable.h>
 
 #include <gtest/gtest.h>
+
+using namespace oac_tree_gui::domainconstants;
 
 namespace oac_tree_gui::test
 {
 
 /**
- * @brief Tests of ApplicationModels class.
+ * @brief Tests for helper methods in domain_helper_tests.h
  */
-class DomainHelperTests : public ::testing::Test
+class DomainHelperTest : public ::testing::Test
 {
 };
 
-TEST_F(DomainHelperTests, GetPluginFileName)
+//! An attempt to catch if our duplication of domain constants diverges from the domain.
+TEST_F(DomainHelperTest, DomainInstructionTypeConstants)
 {
-  if (mvvm::IsLinuxHost())
+  const std::vector<std::string> domain_types = {
+      kChoiceInstructionType,        kConditionInstructionType,
+      kCopyInstructionType,          kDecrementInstructionType,
+      kEqualsInstructionType,        kFallbackInstructionType,
+      kForceSuccessInstructionType,  kIncludeInstructionType,
+      kIncrementInstructionType,     kInputInstructionType,
+      kInverterInstructionType,      kListenInstructionType,
+      kMessageInstructionType,       kOutputInstructionType,
+      kParallelInstructionType,      kRepeatInstructionType,
+      kSequenceInstructionType,      kUserChoiceInstructionType,
+      kVariableResetInstructionType, kWaitInstructionType,
+      kLessThanInstructionType,      kLessThanOrEqualInstructionType,
+      kGreaterThanInstructionType,   kGreaterThanOrEqualInstructionType,
+      kSucceedInstructionType,       kFailedInstructionType};
+
+  for (const auto& domain_type : domain_types)
   {
-    EXPECT_EQ(GetPluginFileName("abc"), std::string("libabc.so"));
-    EXPECT_EQ(GetPluginFileName("libabc"), std::string("libabc.so"));
-    EXPECT_EQ(GetPluginFileName("abc.so"), std::string("libabc.so"));
-    EXPECT_EQ(GetPluginFileName("libabc.so"), std::string("libabc.so"));
-    EXPECT_EQ(GetPluginFileName("abc.dylib"), std::string("libabc.so"));
-    EXPECT_EQ(GetPluginFileName("libabc.dylib"), std::string("libabc.so"));
+    EXPECT_EQ(CreateDomainInstruction(domain_type)->GetType(), domain_type);
+  }
+}
+
+//! An attempt to catch if our duplication of domain constants diverges from the domain.
+//! Testing instructions in sup-sequencer-epics-plugin.
+TEST_F(DomainHelperTest, SequencerPluginEpicsDomainInstructionTypeConstants)
+{
+  if (!IsSequencerPluginEpicsAvailable())
+  {
+    GTEST_SKIP();
   }
 
-  if (mvvm::IsMacHost())
+  const std::vector<std::string> domain_types = {
+      kChannelAccessReadInstructionType, kChannelAccessWriteInstructionType,
+      kPvAccessReadInstructionType,      kPvAccessWriteInstructionType,
+      kRPCClientInstructionType,         kLogInstructionType};
+  for (const auto& domain_type : domain_types)
   {
-    EXPECT_EQ(GetPluginFileName("abc"), std::string("libabc.dylib"));
-    EXPECT_EQ(GetPluginFileName("libabc"), std::string("libabc.dylib"));
-    EXPECT_EQ(GetPluginFileName("abc.so"), std::string("libabc.dylib"));
-    EXPECT_EQ(GetPluginFileName("libabc.so"), std::string("libabc.dylib"));
-    EXPECT_EQ(GetPluginFileName("abc.dylib"), std::string("libabc.dylib"));
-    EXPECT_EQ(GetPluginFileName("libabc.dylib"), std::string("libabc.dylib"));
+    EXPECT_EQ(CreateDomainInstruction(domain_type)->GetType(), domain_type);
   }
+}
+
+//! An attempt to catch if our duplication of domain constants diverges from the domain.
+TEST_F(DomainHelperTest, DomainVariableTypeConstants)
+{
+  const std::vector<std::string> domain_types = {kFileVariableType, kLocalVariableType};
+
+  for (const auto& domain_type : domain_types)
+  {
+    EXPECT_EQ(CreateDomainVariable(domain_type)->GetType(), domain_type);
+  }
+}
+
+//! An attempt to catch if our duplication of domain constants diverges from the domain.
+//! Testing variables in sup-sequencer-epics-plugin.
+TEST_F(DomainHelperTest, SequencerPluginEpicsDomainVariableTypeConstants)
+{
+  if (!IsSequencerPluginEpicsAvailable())
+  {
+    GTEST_SKIP();
+  }
+
+  const std::vector<std::string> domain_types = {
+      kChannelAccessVariableType, kPvAccessClientVariableType, kPvAccessServerVariableType};
+
+  for (const auto& domain_type : domain_types)
+  {
+    EXPECT_EQ(CreateDomainVariable(domain_type)->GetType(), domain_type);
+  }
+}
+
+TEST_F(DomainHelperTest, DialogMetagadata)
+{
+  const sup::dto::AnyValue anyvalue;
+  EXPECT_FALSE(IsSelectTextDialog(anyvalue));
+  EXPECT_FALSE(IsMessageBoxDialog(anyvalue));
+
+  {  // pretending it is metadata representing test selection
+    auto metadata = sup::oac_tree::CreateUserChoiceMetadata();
+    metadata.AddMember(sup::oac_tree::Constants::USER_CHOICES_TEXT_NAME, "description");
+    metadata.AddMember(sup::oac_tree::Constants::USER_CHOICES_DIALOG_TYPE_NAME,
+                       {sup::dto::UnsignedInteger32Type, sup::oac_tree::dialog_type::kSelection});
+
+    EXPECT_TRUE(IsSelectTextDialog(metadata));
+    EXPECT_FALSE(IsMessageBoxDialog(metadata));
+  }
+
+  {  // pretending it is metadata representing ok/cancel choice
+    auto metadata = sup::oac_tree::CreateUserChoiceMetadata();
+    metadata.AddMember(sup::oac_tree::Constants::USER_CHOICES_TEXT_NAME, "description");
+    metadata.AddMember(
+        sup::oac_tree::Constants::USER_CHOICES_DIALOG_TYPE_NAME,
+        {sup::dto::UnsignedInteger32Type, sup::oac_tree::dialog_type::kConfirmation});
+
+    EXPECT_FALSE(IsSelectTextDialog(metadata));
+    EXPECT_TRUE(IsMessageBoxDialog(metadata));
+  }
+}
+
+TEST_F(DomainHelperTest, GlobalDomainObjectTypeRegistry)
+{
+  const std::string undefined("undefined");
+
+  // instructions and variables from core
+
+  EXPECT_EQ(GlobalDomainObjectTypeRegistry()
+                .GetPluginName(domainconstants::kWaitInstructionType)
+                .value_or(undefined),
+            domainconstants::kCorePluginName);
+
+  EXPECT_EQ(GlobalDomainObjectTypeRegistry()
+                .GetPluginName(domainconstants::kLocalVariableType)
+                .value_or(undefined),
+            domainconstants::kCorePluginName);
+
+  if (!IsSequencerPluginEpicsAvailable())
+  {
+    GTEST_SKIP();
+  }
+
+  // instructions and variables from EPICS libs
+
+  EXPECT_EQ(GlobalDomainObjectTypeRegistry()
+                .GetPluginName(domainconstants::kChannelAccessReadInstructionType)
+                .value_or(undefined),
+            domainconstants::kEpicsCAPluginName);
+
+  EXPECT_EQ(GlobalDomainObjectTypeRegistry()
+                .GetPluginName(domainconstants::kPvAccessClientVariableType)
+                .value_or(undefined),
+            domainconstants::kEpicsPVXSPluginName);
 }
 
 }  // namespace oac_tree_gui::test
