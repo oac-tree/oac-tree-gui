a2ad3f58a207449d919f7c8f8b3881e72e5d4ca9
Gennady Pospelov <gennady.pospelov@iter.org>
2025-08-21 16:09:41 +0200

    Implement unit tests for DomainPluginService

---
+++ test/testoac-tree-gui-views/sequencer_main_window_context_tests.cpp
@@ -0,0 +1,81 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP oac-tree
+ *
+ * Description   : Integrated development environment for oac-tree procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2025 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file LICENSE located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "oac_tree_gui/mainwindow/sequencer_main_window_context.h"
+
+#include <oac_tree_gui/domain/domain_library_loader.h>
+#include <oac_tree_gui/domain/domain_object_type_registry.h>
+#include <oac_tree_gui/domain/domain_plugin_service.h>
+
+#include <sup/gui/app/app_command_service.h>
+
+#include <gtest/gtest.h>
+
+namespace oac_tree_gui::test
+{
+
+class SequencerMainWindowContextTest : public ::testing::Test
+{
+};
+
+TEST_F(SequencerMainWindowContextTest, InitialState)
+{
+  SequencerMainWindowContext context;
+
+  EXPECT_NE(dynamic_cast<sup::gui::AppCommandService*>(&context.GetCommandService()), nullptr);
+
+  auto plugin_service =
+      dynamic_cast<DomainPluginService<DomainLibraryLoader, DomainObjectTypeRegistry>*>(
+          &context.GetDomainPluginService());
+  ASSERT_NE(plugin_service, nullptr);
+  EXPECT_EQ(plugin_service->GetLoadedPlugins().size(), 0);
+  EXPECT_EQ(plugin_service->GetPluginLoadInfo().size(), 0);
+
+  EXPECT_TRUE(plugin_service->GetObjectNames(domainconstants::kCorePluginName).empty());
+}
+
+TEST_F(SequencerMainWindowContextTest, LoadNonExistingPlugin)
+{
+  SequencerMainWindowContext context;
+
+  EXPECT_NE(dynamic_cast<sup::gui::AppCommandService*>(&context.GetCommandService()), nullptr);
+
+  auto plugin_service =
+      dynamic_cast<DomainPluginService<DomainLibraryLoader, DomainObjectTypeRegistry>*>(
+          &context.GetDomainPluginService());
+  ASSERT_NE(plugin_service, nullptr);
+
+  plugin_service->LoadPlugins({"non-existing-plugin"});
+
+  EXPECT_EQ(plugin_service->GetLoadedPlugins().size(), 0);
+  const std::vector<std::pair<std::string, bool>> expected_info = {{"non-existing-plugin", false}};
+  EXPECT_EQ(plugin_service->GetPluginLoadInfo(), expected_info);
+
+  // check that core instruction and variables are there
+  auto object_names = plugin_service->GetObjectNames(domainconstants::kCorePluginName);
+  EXPECT_NE(std::find(object_names.begin(), object_names.end(),
+                      domainconstants::kSequenceInstructionType),
+            object_names.end());
+  EXPECT_NE(
+      std::find(object_names.begin(), object_names.end(), domainconstants::kWaitInstructionType),
+      object_names.end());
+}
+
+}  // namespace oac_tree_gui::test
