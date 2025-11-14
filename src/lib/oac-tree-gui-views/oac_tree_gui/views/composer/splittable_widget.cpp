73915f9358a1f72899d0dd84365512298cc6d95e
Gennady Pospelov <gennady.pospelov@iter.org>
2025-11-14 11:21:07 +0100

    Introduce SplittableWidget

---
+++ src/lib/oac-tree-gui-views/oac_tree_gui/views/composer/splittable_widget.cpp
@@ -0,0 +1,66 @@
+/******************************************************************************
+ *
+ * Project       : Graphical User Interface for SUP oac-tree
+ *
+ * Description   : Integrated development environment for oac-tree procedures
+ *
+ * Author        : Gennady Pospelov (IO)
+ *
+ * Copyright (c) : 2010-2025 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file LICENSE located in the top level directory
+ * of the distribution package.
+ *****************************************************************************/
+
+#include "splittable_widget.h"
+
+#include <oac_tree_gui/core/exceptions.h>
+
+#include <QSplitter>
+#include <QVBoxLayout>
+
+namespace oac_tree_gui
+{
+
+SplittableWidget::SplittableWidget(CreateWidgetCallback callback, QWidget* parent_widget)
+    : QWidget(parent_widget), m_callback(std::move(callback)), m_splitter(new QSplitter)
+{
+  if (!m_callback)
+  {
+    throw RuntimeException("Callback is not defined");
+  }
+
+  auto layout = new QVBoxLayout(this);
+  layout->setContentsMargins(0, 0, 0, 0);
+  layout->setSpacing(0);
+  layout->addWidget(m_splitter);
+}
+
+SplittableWidget::~SplittableWidget() = default;
+
+QSplitter* SplittableWidget::GetSplitter() const
+{
+  return m_splitter;
+}
+
+void SplittableWidget::AddWidget(QWidget* after_widget)
+{
+  auto new_widget = m_callback();
+  if (after_widget == nullptr)
+  {
+    m_splitter->addWidget(new_widget.release());
+  }
+  else
+  {
+    const std::int32_t index = m_splitter->indexOf(after_widget);
+    m_splitter->insertWidget(index + 1, new_widget.release());
+  }
+}
+
+}  // namespace oac_tree_gui
