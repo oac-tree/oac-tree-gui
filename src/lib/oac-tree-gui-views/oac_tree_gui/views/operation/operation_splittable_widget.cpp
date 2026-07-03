d342c0525a6c824045516769853d853973059448
Gennady Pospelov <gennady.pospelov@iter.org>
2026-07-03 13:16:17 +0200

    Implement basics of OperationSplittableWidget

--- src/lib/oac-tree-gui-views/oac_tree_gui/views/operation/operation_splittable_widget.cpp
+++ src/lib/oac-tree-gui-views/oac_tree_gui/views/operation/operation_splittable_widget.cpp
@@ -20,16 +20,85 @@
 
 #include "operation_splittable_widget.h"
 
+#include "operation_tab_widget.h"
+
+#include <oac_tree_gui/model/job_item.h>
+#include <oac_tree_gui/model/job_model.h>
+
+#include <mvvm/utils/container_utils.h>
+
+#include <sup/gui/views/dtoeditor/splittable_combo_panel.h>
+#include <sup/gui/views/dtoeditor/splittable_editor_controller.h>
+
+#include <QSplitter>
 #include <QVBoxLayout>
 
 namespace oac_tree_gui
 {
 
-OperationSplittableWidget::OperationSplittableWidget(QWidget* parent_widget) : QWidget(parent_widget)
+OperationSplittableWidget::OperationSplittableWidget(QWidget* parent_widget)
+    : QWidget(parent_widget)
+    , m_splitter(new QSplitter)
+    , m_splitter_controller(CreateSplitterController())
 {
   auto layout = new QVBoxLayout(this);
   layout->setContentsMargins(0, 0, 0, 0);
   layout->setSpacing(0);
+  layout->addWidget(m_splitter);
+
+  connect(m_splitter_controller.get(),
+          &sup::gui::SplittableEditorController::focusPanelSelectionChanged, this,
+          &OperationSplittableWidget::focusWidgetJobSelectionChanged);
+
+  CreatePanel();
+}
+
+OperationSplittableWidget::~OperationSplittableWidget() = default;
+
+void OperationSplittableWidget::SetModel(JobModel* model)
+{
+  m_model = model;
+  m_splitter_controller->SetModel(model);
+}
+
+void OperationSplittableWidget::SetCurrentJob(JobItem* job_item)
+{
+  if (auto focus_widget = m_splitter_controller->GetFocusWidget(); focus_widget)
+  {
+    focus_widget->SetItem(job_item);
+  }
+}
+
+sup::gui::SplittableComboPanel* OperationSplittableWidget::CreatePanel()
+{
+  return m_splitter_controller->CreatePanel();
+}
+
+sup::gui::SplittableEditorController* OperationSplittableWidget::GetController() const
+{
+  return m_splitter_controller.get();
+}
+
+std::unique_ptr<sup::gui::SplittableEditorController>
+OperationSplittableWidget::CreateSplitterController() const
+{
+  auto factory_func = [this]() -> std::unique_ptr<sup::gui::SplittableComboPanel>
+  {
+    auto main_editor = std::make_unique<OperationTabWidget>();
+
+    auto item_list_callback = [this]() -> std::vector<mvvm::SessionItem*>
+    {
+      return m_model == nullptr
+                 ? std::vector<mvvm::SessionItem*>()
+                 : mvvm::utils::CastItems<mvvm::SessionItem>(m_model->GetJobItems());
+    };
+
+    auto result = std::make_unique<sup::gui::SplittableComboPanel>(std::move(main_editor),
+                                                                   item_list_callback);
+    result->SetModel(m_model);
+    return result;
+  };
+  return std::make_unique<sup::gui::SplittableEditorController>(m_splitter, factory_func);
 }
 
 }  // namespace oac_tree_gui
--- src/lib/oac-tree-gui-views/oac_tree_gui/views/operation/operation_splittable_widget.h
+++ src/lib/oac-tree-gui-views/oac_tree_gui/views/operation/operation_splittable_widget.h
@@ -22,18 +22,74 @@
 #define OAC_TREE_GUI_VIEWS_OPERATION_OPERATION_SPLITTABLE_WIDGET_H_
 
 #include <QWidget>
+#include <memory>
+
+class QSplitter;
+
+namespace mvvm
+{
+class ItemSelection;
+}
+
+namespace sup::gui
+{
+class SplittableEditorController;
+class SplittableComboPanel;
+}  // namespace sup::gui
 
 namespace oac_tree_gui
 {
 
-//! Placeholder widget for future operation splittable development.
+class JobModel;
+class JobItem;
 
+/**
+ * @brief The OperationSplittableWidget holds multiple splittable OperationTabWidgets.
+ */
 class OperationSplittableWidget : public QWidget
 {
   Q_OBJECT
 
 public:
   explicit OperationSplittableWidget(QWidget* parent_widget = nullptr);
+  ~OperationSplittableWidget() override;
+
+  OperationSplittableWidget(const OperationSplittableWidget&) = delete;
+  OperationSplittableWidget& operator=(const OperationSplittableWidget&) = delete;
+  OperationSplittableWidget(OperationSplittableWidget&&) = delete;
+  OperationSplittableWidget& operator=(OperationSplittableWidget&&) = delete;
+
+  void SetModel(JobModel* model);
+
+  /**
+   * @brief Sets the job to be shown in the currently focused panel.
+   */
+  void SetCurrentJob(JobItem* job_item);
+
+  /**
+   * @brief Adds panel to the splitter.
+   */
+  sup::gui::SplittableComboPanel* CreatePanel();
+
+  /**
+   * @brief Returns controller of the editor, which allows to manipulate panels and their content.
+   *
+   * Used for testing.
+   */
+  sup::gui::SplittableEditorController* GetController() const;
+
+signals:
+  /**
+   * @brief Notifies that the job selection in the focus widget has changed.
+   */
+  void focusWidgetJobSelectionChanged(const mvvm::ItemSelection& selection);
+
+private:
+  std::unique_ptr<sup::gui::SplittableEditorController> CreateSplitterController() const;
+
+  QSplitter* m_splitter{nullptr};
+  JobModel* m_model{nullptr};
+  std::unique_ptr<sup::gui::SplittableEditorController> m_splitter_controller;
 };
 
 }  // namespace oac_tree_gui
